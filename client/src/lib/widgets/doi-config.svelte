df668dccb20dee120ca10a40c8a8cf89ae223ca6
Mahog <6837928+Mahog@users.noreply.github.com>
2021-08-31 10:40:57 +0200

    make doi config width a parameter

--- client/src/lib/widgets/doi-config.svelte
+++ client/src/lib/widgets/doi-config.svelte
@@ -1,10 +1,13 @@
 <script lang="typescript">
 
 export let title: string;
+export let width = -1;
+
+const maxWidth = width < 0 ? "" : `max-width:${width}px`;
 
 </script>
 
-<div class="doi-config">
+<div class="doi-config" style={maxWidth}>
   <h2>{ title }</h2>
   <slot></slot>
 </div>
@@ -15,6 +18,7 @@ export let title: string;
     min-width: 300px;
     min-height: 150px;
     padding: 1rem 1.5rem 1.5rem;
+    overflow: auto;
   }
   div.doi-config h2 {
     margin: 10px 0;
