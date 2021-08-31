ccecfe4979f027866694143b9fc8ede4df3fcb77
Mahog <6837928+Mahog@users.noreply.github.com>
2021-08-31 11:36:23 +0200

    add vl histogram renderers

---
+++ client/src/lib/widgets/vega-lite-plot.svelte
@@ -0,0 +1,27 @@
+<script lang="typescript">
+  import { afterUpdate } from "svelte";
+
+  export let id: string;
+  export let spec: Record<string, unknown>;
+
+  let vegaEmbed;
+
+  afterUpdate(() => {
+    vegaEmbed = (window as any).vegaEmbed;
+    if (vegaEmbed === undefined) {
+      console.error("vega embed is undefined");
+      return;
+    }
+    console.log(spec);
+    vegaEmbed.embed(`#${id}-vega-container`, spec, { actions: false });
+  });
+
+</script>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/vega"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vega-lite"></script>
+	<script src="https://cdn.jsdelivr.net/npm/vega-embed"></script>
+</svelte:head>
+
+<div id="{id}-vega-container"></div>
