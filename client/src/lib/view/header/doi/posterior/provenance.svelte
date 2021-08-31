1842a1fdccae62a54a8e1208f21b8a2697ddd0cd
Mahog <6837928+Mahog@users.noreply.github.com>
2021-08-31 10:28:33 +0200

    add provenance interest overlay

---
+++ client/src/lib/view/header/doi/posterior/provenance.svelte
@@ -0,0 +1,20 @@
+<script>
+import { interactionWeights } from "$lib/state/interaction-technique-weights";
+
+import Column from "$lib/widgets/column.svelte";
+import DoiConfig from "$lib/widgets/doi-config.svelte";
+import WeightedValues from "$lib/widgets/weighted-values.svelte";
+
+</script>
+
+
+<DoiConfig title="Configure Provenance">
+  <Column>
+    <h3>Assign weights to interactions</h3>
+    <WeightedValues
+      group="interaction-technique-weights"
+      valueWeights={ $interactionWeights }
+      totalSize={ 400 }
+    />
+  </Column>
+</DoiConfig>
