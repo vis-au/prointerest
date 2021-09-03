5b27f15ebed51b09b10170c82433774924d47a7f
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-03 10:49:34 +0200

    add basic db connection

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, abort, jsonify, request
+from flask import Flask, jsonify
+import duckdb
 import random
 
 app = Flask(__name__)
@@ -6,11 +7,41 @@ app = Flask(__name__)
 CHUNK_SIZE = 100
 DIMENSIONS = 5
 
+# database constants
+PATH = "./data/nyc_taxis.shuffled_full.csv.gz"
+TABLE = "data"
+PROCESSED = "processed" # name of database containing ids of processed data
+ID = "tripID"
+ID_INDEX = 0
 
-def cors_response(payload):
-  response = jsonify(payload)
-  response.headers.add("Access-Control-Allow-Origin", "*")
-  return response
+cursor = duckdb.connect() # database connection
+
+
+def initialize_db():
+  cursor.execute(f"CREATE VIEW {TABLE} AS SELECT * FROM read_csv_auto('{PATH}')")
+  cursor.execute(f"CREATE TABLE {PROCESSED} ({ID} VARCHAR UNIQUE PRIMARY KEY)")
+
+
+def mark_ids_plotted(ids: list):
+  values = "('"+"'),('".join(ids)+"')"
+  query = f"INSERT INTO {PROCESSED} ({ID}) VALUES {values}"
+  cursor.execute(query)
+
+
+def get_next_chunk_from_db():
+  query = f"SELECT * FROM {TABLE} WHERE {ID} NOT IN (SELECT {ID} FROM {PROCESSED}) LIMIT {CHUNK_SIZE}"
+  next_chunk = cursor.execute(query).fetchall()
+  ids = [str(item[ID_INDEX]) for item in next_chunk]
+  mark_ids_plotted(ids)
+
+  return next_chunk
+
+
+def get_dimensions_in_data():
+  query = f"SELECT * FROM {TABLE} LIMIT 1"
+  item = cursor.execute(query).fetchdf()
+  dimensions = list(item.columns)
+  return dimensions
 
 
 def get_random_sample():
@@ -22,6 +53,12 @@ def get_random_dims():
 
 
 
+def cors_response(payload):
+  response = jsonify(payload)
+  response.headers.add("Access-Control-Allow-Origin", "*")
+  return response
+
+
 @app.route("/")
 def hello_world():
   return "Ok. Flask server successfully launched."
@@ -29,19 +66,17 @@ def hello_world():
 
 @app.route("/dimensions", methods=["GET"])
 def get_dimensions():
-  return cors_response(get_random_dims())
+  dims = get_dimensions_in_data()
+  return cors_response(dims)
 
 
 @app.route("/next_chunk", methods=["GET"])
 def get_next_chunk():
-  return cors_response(get_random_sample())
-
-
-# @app.route("/create_pipeline/<id>", methods=["GET"])
-# def create_pipeline(id):
-#   PIPELINES[str(id)] = Pipeline(get_pipeline_config(request))
-#   return cors_response("ok")
+  # chunk = get_random_sample()
+  chunk = get_next_chunk_from_db()
+  return cors_response(chunk)
 
 
 if __name__ == "__main__":
+  initialize_db()
   app.run(debug=True)
