eae3748db58e5cfefa6d0be091f3a058587c76d3
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-06 18:09:27 +0200

    output top 100 interesting items when computing doi

--- client/src/lib/state/suggested-items.ts
+++ client/src/lib/state/suggested-items.ts
@@ -3,21 +3,24 @@ import type DataItem from '$lib/types/data-item';
 import type { SuggestionMode } from '$lib/types/indicate-mode';
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
-import { activeIndicateMode } from './active-indicate-mode';
+import { activeSuggestionMode } from './active-indicate-mode';
 import { interestingItems } from './interesting-items';
 import { quadtree } from './quadtree';
+import { top100Ids } from './latest-doi-values';
 
 const guide = new GuidanceProvider();
 
 let currentlyInterestItems: DataItem[] = [];
+let currentTop100Ids: number[];
 let currentMode: SuggestionMode = null;
 
 export const suggestedItems: Writable<DataItem[]> = writable([]);
 
 function updateSuggestedItems() {
-	console.log(currentlyInterestItems);
 	if (currentMode === 'explored') {
 		suggestedItems.set(currentlyInterestItems);
+	} else if (currentMode === "interesting") {
+		console.log(currentTop100Ids);
 	} else if (currentMode === 'similar') {
 		suggestedItems.set(guide.getItemsSimilarToInterest(currentlyInterestItems));
 	} else if (currentMode === 'dissimilar') {
@@ -30,11 +33,16 @@ interestingItems.subscribe((newItems) => {
 	updateSuggestedItems();
 });
 
-activeIndicateMode.subscribe((newMode) => {
+activeSuggestionMode.subscribe((newMode) => {
 	currentMode = newMode;
 	updateSuggestedItems();
 });
 
 quadtree.subscribe((newTree) => {
 	guide.processedDataspace = newTree.data();
+});
+
+top100Ids.subscribe((items) => {
+	currentTop100Ids = items;
+	updateSuggestedItems();
 });
--- client/src/lib/view/header/header.svelte
+++ client/src/lib/view/header/header.svelte
@@ -1,9 +1,21 @@
 <script lang="typescript">
+	import { activeDoiValues } from '$lib/state/latest-doi-values';
+	import { quadtree } from '$lib/state/quadtree';
+	import { getDoiValues } from '$lib/util/requests';
 	import Row from '$lib/widgets/row.svelte';
+
+	import ControlButton from '../main/control-button.svelte';
 	import PosteriorWeights from './posterior-weights.svelte';
 	import PriorWeights from './prior-weights.svelte';
 
 	export let height;
+
+	async function evaluateInterest() {
+		const doiValues = await getDoiValues($quadtree.data());
+		const map = new Map<number, number>();
+		doiValues.forEach(pair => map.set(pair[0], pair[1]));
+		$activeDoiValues = map;
+	}
 </script>
 
 <header>
@@ -13,6 +25,9 @@
 	<Row id="doi-configuration" style="{height}px">
 		<PriorWeights />
 		<PosteriorWeights />
+		<ControlButton style="background:limegreen;margin:0 10px;padding:5px 10px" on:click={ evaluateInterest }>
+			Compute
+		</ControlButton>
 	</Row>
 </header>
 
