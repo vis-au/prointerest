9c6e1bd916170732f7ea886305b563ac85d2076d
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-07 15:23:29 +0200

    allow hiding inactive options from widget

--- client/src/lib/widgets/options.svelte
+++ client/src/lib/widgets/options.svelte
@@ -1,23 +1,56 @@
 <script lang="typescript">
+import Dropdown from "./dropdown.svelte";
+
+
   export let id = "";
   export let className = "";
   export let style = "";
   export let options: string[];
   export let activeOptions: Record<string, boolean>;
+  export let showInactive = true;
+
+  $: inactiveOptions = options.filter(o => !activeOptions[o]);
+
+  function select(option: string) {
+    activeOptions[option] = true;
+    activeOptions = activeOptions;
+  }
 </script>
 
 <div {id} class="options {className}" {style}>
-  {#each options as option}
-    <div class="option">
-      <input
-        id="options-list-{option}"
-        type="checkbox"
-        value={option}
-        bind:checked={ activeOptions[option] }
-      />
-      <label for="options-list-{option}">{option}</label>
-    </div>
-  {/each}
+  {#if showInactive}
+    {#each options as option}
+      <div class="option">
+        <input
+          id="options-list-{option}"
+          type="checkbox"
+          value={option}
+          bind:checked={ activeOptions[option] }
+        />
+        <label for="options-list-{option}">{option}</label>
+      </div>
+    {/each}
+
+  {:else}
+    <Dropdown id="inactive-options" selectedValue=null style="margin-right:20px">
+      <option disabled value=null>add histogram</option>
+      {#each inactiveOptions as option}
+        <option value={option} on:click={ () => select(option) }>{option}</option>
+      {/each}
+    </Dropdown>
+
+    {#each Object.keys(activeOptions) as option}
+      <div class="option">
+        <input
+          id="options-list-{option}"
+          type="checkbox"
+          value={option}
+          bind:checked={ activeOptions[option] }
+        />
+        <label for="options-list-{option}">{option}</label>
+      </div>
+    {/each}
+  {/if}
 </div>
 
 <style>
