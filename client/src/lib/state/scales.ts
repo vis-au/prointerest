963949ecddd0e7b172fbfde7fb60cfdadcfd6750
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-08 09:42:52 +0200

    get scale extents from server instead of computing

--- client/src/lib/state/scales.ts
+++ client/src/lib/state/scales.ts
@@ -1,59 +1,45 @@
-import type DataItem from "$lib/types/data-item";
-import { extent } from "d3";
+import { getDimensionExtent } from "$lib/util/requests";
 import { scaleLinear } from "d3-scale";
 import { writable } from "svelte/store";
 import { activeViewEncodings } from "./active-view-encodings";
-import { dimensions } from "./processed-data";
-import { quadtree } from "./quadtree";
 
-let currentProcessedData: DataItem[] = [];
-let currentDimensions: string[] = [];
 let currentScaleX = scaleLinear();
 let currentScaleY = scaleLinear();
-let xIndex = -1;
-let yIndex = -1;
+let xDimension: string = null;
+let yDimension: string = null;
 const margin = 0.05;
 
 export const scaleX = writable(currentScaleX);
 export const scaleY = writable(currentScaleY);
 
-function updateScales() {
-  const xValues =
-    currentProcessedData.length === 0 ? [0, 1] : currentProcessedData.map((d) => d.values[xIndex]);
+async function updateScales() {
+  if (xDimension === null || yDimension === null) {
+    return;
+  }
 
-  const yValues =
-    currentProcessedData.length === 0 ? [0, 1] : currentProcessedData.map((d) => d.values[yIndex]);
+  const xExtent = await getDimensionExtent(xDimension);
+  const yExtent = await getDimensionExtent(yDimension);
 
-  let xExtent = extent(xValues);
-  const xMargin = (xExtent[1] - xExtent[0]) * margin;
-  xExtent = [xExtent[0] - xMargin, xExtent[1] + xMargin];
+  const xMargin = (xExtent.max - xExtent.min) * margin;
+  xExtent.min = xExtent.min - xMargin;
+  xExtent.max = xExtent.max + xMargin;
 
-  let yExtent = extent(yValues);
-  const yMargin = (yExtent[1] - yExtent[0]) * margin;
-  yExtent = [yExtent[0] - yMargin, yExtent[1] + yMargin];
+  const yMargin = (yExtent.max - yExtent.min) * margin;
+  yExtent.min = yExtent.min - yMargin;
+  yExtent.max = yExtent.max + yMargin;
 
-  currentScaleX.domain(xExtent);
-  currentScaleY.domain(yExtent.reverse());
+  currentScaleX.domain([xExtent.min, xExtent.max]);
+  currentScaleY.domain([yExtent.min, yExtent.max].reverse());
 
   scaleX.set(currentScaleX);
   scaleY.set(currentScaleY);
 }
 
-setTimeout(() => {
-  quadtree.subscribe((newTree) => {
-    currentProcessedData = newTree.data();
-
-    updateScales();
-  });
-}, 0);
-
 activeViewEncodings.subscribe((newEncodings) => {
-  xIndex = currentDimensions.indexOf(newEncodings.x);
-  yIndex = currentDimensions.indexOf(newEncodings.y);
+  xDimension = newEncodings.x;
+  yDimension = newEncodings.y;
   updateScales();
 });
-
-dimensions.subscribe((newDims) => (currentDimensions = newDims));
 
 scaleX.subscribe((scale) => (currentScaleX = scale));
 scaleY.subscribe((scale) => (currentScaleY = scale));
