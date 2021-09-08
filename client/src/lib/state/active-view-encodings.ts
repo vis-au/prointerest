a07fcc822ac5da4de7401b8f32bd7f057633db30
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-08 08:02:41 +0200

    x and y dimensions are interesting by default

--- client/src/lib/state/active-view-encodings.ts
+++ client/src/lib/state/active-view-encodings.ts
@@ -2,6 +2,8 @@ import type { Encodings } from "$lib/types/encodings";
 import { sendAxisDimension } from "$lib/util/requests";
 import type { Writable } from "svelte/store";
 import { writable } from "svelte/store";
+import { interestingDimensions } from "./interesting-dimensions";
+
 
 export const activeViewEncodings: Writable<Encodings> = writable({
   x: null,
@@ -10,6 +12,18 @@ export const activeViewEncodings: Writable<Encodings> = writable({
 });
 
 activeViewEncodings.subscribe((newEncodings) => {
-  sendAxisDimension("x", newEncodings.x);
-  sendAxisDimension("y", newEncodings.y);
+  if (newEncodings.x !== null) {
+    sendAxisDimension("x", newEncodings.x);
+    interestingDimensions.update((dims) => {
+      dims[newEncodings.x] = true;
+      return dims;
+    });
+  }
+  if (newEncodings.y !== null) {
+    sendAxisDimension("y", newEncodings.y);
+    interestingDimensions.update((dims) => {
+      dims[newEncodings.y] = true;
+      return dims;
+    });
+  }
 });
--- client/src/lib/state/interesting-dimensions.ts
+++ client/src/lib/state/interesting-dimensions.ts
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 import { dimensions } from "./processed-data";
 
 const currentSelection: Record<string, boolean> = {};
-export const dimensionInterestRecord = writable(currentSelection);
+export const interestingDimensions = writable(currentSelection);
 
 dimensions.subscribe(dims => {
   dims.map(dim => currentSelection[dim] = false);
