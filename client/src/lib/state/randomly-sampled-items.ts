6ea568b5f06644d75e1c6e4ed42e39e9b95b3051
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-09 17:58:02 +0200

    fix selection in layered histograms now survives panel toggle

--- client/src/lib/state/randomly-sampled-items.ts
+++ client/src/lib/state/randomly-sampled-items.ts
@@ -1,13 +1,15 @@
 import type DataItem from "$lib/types/data-item";
 import { quadtree } from "./quadtree";
 import { writable } from "svelte/store";
+import type { Quadtree } from "d3-quadtree";
 
 const iterationsBetweenUpdates = 10;
 const sampleSize = 50000
-let iteration = 0;
 
-let processedItems: DataItem[] = [];
+let iteration = 0;
 let probability = 1;
+let currentQuadtree: Quadtree<DataItem> = null;
+let currentProcessedItems: DataItem[] = [];
 
 let sampledItems = [] as DataItem[];
 export const randomlySampledItems = writable(sampledItems);
@@ -20,15 +22,16 @@ function sample() {
 };
 
 quadtree.subscribe(newTree => {
-  processedItems = newTree.data();
-  probability = sampleSize / processedItems.length;
-
-  if (iteration % iterationsBetweenUpdates === 0) {
-    sampledItems = processedItems.filter(sample);
+  // check if quadtree has changed (for exaxmple when opening the secondary view panel)
+  if (iteration % iterationsBetweenUpdates === 0 || currentQuadtree !== newTree) {
+    currentProcessedItems = newTree.data();
+    probability = sampleSize / currentProcessedItems.length;
+    sampledItems = currentProcessedItems.filter(sample);
     randomlySampledItems.set(sampledItems);
+    currentQuadtree = newTree;
   }
 
-  if (processedItems.length > 0) {
+  if (currentProcessedItems.length > 0) {
     iteration += 1;
   }
 });
