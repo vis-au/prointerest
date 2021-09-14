b925f0693dd7ad30ea5aefca8014410c830de40b
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-14 17:56:06 +0200

    refine doi component class

--- server/doi_component.py
+++ server/doi_component.py
@@ -1,7 +1,64 @@
+import numpy as np
+from sklearn.svm import LinearSVR
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
 class DoiComponent:
+  '''
+  Describes a general component of a progressive degree-of-interest function. It can either be prior
+  or posterior component. Prior components derive the degree-of-interest for each data item in the
+  context of other data without considering user behavior. Posterior components on the other hand
+  derive the degree-of-interest for each data item by also considering user interactions, for
+  example by measuring the similarity to a focus point set by the user or by.
+  '''
   def __init__(self) -> None:
+    self.weights: dict[str, float] = {}
+    self.current_interest = np.empty(shape=(0, 2))
+    self.predictor: LinearSVR = make_pipeline(StandardScaler(), LinearSVR(random_state=0, tol=1e-5))
+
+
+  def set_components(self, weights: dict[str, float]):
+    '''
+    Specify components of the doi function, such as weights. Expects a dictionary that for each
+    component specifies a numeric value.
+    '''
+    self.weights = weights
+
+
+  def update(self, data):
+    '''
+    Send more information to the doi function based on the progression. This could for instance be
+    a set of adjusted function parameters.
+    '''
     pass
-  def update():
+
+
+  def evaluate_doi(self, X):
+    '''
+    Compute the degree of interest over a matrix of shape (n, m) using the degree-of-interest
+    function of this object. Outputs a vector y of shape (n, 1), containing the actual doi for
+    each item.
+    '''
     pass
-  def get_interest(item_id: str) -> float:
+
+
+  def predict_doi(self, X):
+    '''
+    Given the state of the doi predictor, predicts the degree of interest for the given a matrix X
+    with shape (n, m) without evaluating the full function. Returns a vector y of shape (n, 1),
+    containing the predicted doi for each item.
+    '''
+    pass
+
+
+  def get_prediction_error(self, X):
+    '''
+    A benchmarking function for the predictor: How accurate are the predictions based on the current
+    model? Takes a matrix X with shape (n, m) as input, then computes both the prediction and the
+    actual doi for this matrix, therefore has high runtime. Outputs the place-wise error as a vector
+    of shape (n, 1).
+    '''
+    pass
+
+  def train(self):
     pass
