e6ccb78cde2c907254a2723658dd8f2a6d296323
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-16 15:21:07 +0200

    move scagnostic weights into scagnostic component

--- server/scagnostics_component.py
+++ server/scagnostics_component.py
@@ -2,22 +2,41 @@ from pyscagnostics import scagnostics
 from doi_component import DoiComponent
 import pandas as pd
 
-
-def _get_mean_scangostics(result_generator):
-    all_results = []
-    for x, y, result in result_generator:
-      measures, _ = result
-      all_results += [measures]
-
-    return pd.DataFrame(all_results).mean()
+SCAGNOSTIC_WEIGHTS = {
+  "outlying": 0.11,
+  "skewed": 0.11,
+  "clumpy": 0.11,
+  "sparse": 0.11,
+  "striated": 0.11,
+  "convex": 0.11,
+  "skinny": 0.11,
+  "stringy": 0.11,
+  "monotonic": 0.11
+}
+SCAGNOSTICS = [
+  "Outlying", "Skewed", "Clumpy", "Sparse", "Striated", "Convex", "Skinny", "Stringy", "Monotonic"
+]
+SCATTERPLOT_AXES: dict[str, str] = {
+  "x": None,
+  "y": None
+}
 
 class ScagnosticsComponent(DoiComponent):
   def __init__(self) -> None:
       super().__init__()
-      self.weights = {}
+      self.weights = SCAGNOSTIC_WEIGHTS
 
   def _compute_scagnostics(self, df):
     return scagnostics(df)
+
+
+  def _get_mean_scangostics(self, result_generator):
+      all_results = []
+      for x, y, result in result_generator:
+        measures, _ = result
+        all_results += [measures]
+
+      return pd.DataFrame(all_results).mean()
 
 
   def compute_doi(self, X: pd.DataFrame):
@@ -29,7 +48,7 @@ class ScagnosticsComponent(DoiComponent):
 
       # compute scagnostics without each item
       generators = X_.apply(lambda item: scagnostics(X_[X_.index != item.name]), axis=1)
-      mean_per_item = generators.apply(_get_mean_scangostics, axis=1)
+      mean_per_item = generators.apply(self._get_mean_scangostics)
 
       total_doi = (mean_per_item - mean_all).mean(axis=1).abs()
       min_doi = total_doi.min()
