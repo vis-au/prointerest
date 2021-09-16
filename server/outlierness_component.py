cf826dfe305a8c5a53fa60c8de6b8b38dfb83cc2
Mahog <6837928+Mahog@users.noreply.github.com>
2021-09-16 13:16:35 +0200

    add outlierness doi component

---
+++ server/outlierness_component.py
@@ -0,0 +1,48 @@
+from doi_component import DoiComponent
+import pandas as pd
+import numpy as np
+
+from sklearn import svm
+from sklearn.covariance import EllipticEnvelope
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+# adapted from https://scikit-learn.org/stable/modules/outlier_detection.html
+class OutliernessComponent(DoiComponent):
+  def __init__(self) -> None:
+    super().__init__()
+    self.outliers_fraction = 0.15
+    self.outlierness_measures = self.generate_measures()
+    self.current_interest: pd.DataFrame = None
+
+
+  def generate_measures(self):
+    return [
+      EllipticEnvelope(contamination=self.outliers_fraction),
+      svm.OneClassSVM(nu=self.outliers_fraction, kernel="rbf", gamma=0.1),
+      IsolationForest(contamination=self.outliers_fraction, random_state=0),
+      # LocalOutlierFactor(n_neighbors=35, contamination=self.outliers_fraction)
+    ]
+
+
+  def compute_doi(self, X: pd.DataFrame):
+    X_ = X.to_numpy()
+    predictions = np.array(
+      [measure.fit(X_).predict(X_) for measure in self.outlierness_measures]
+    )
+    sum = predictions.sum(axis=0)
+    scaled = sum / len(predictions)
+
+    return scaled
+
+
+  def train(self, X: pd.DataFrame):
+    X_ = X.drop(columns=["id"])
+    training_data = X_.to_numpy()
+    training_labels = self.compute_doi(X_)
+
+    return self.predictor.fit(training_data, training_labels)
+
+
+  def predict_doi(self, X: pd.DataFrame):
+    return self.predictor.predict(X)
--- server/provenance_component.py
+++ server/provenance_component.py
@@ -8,9 +8,7 @@ class ProvenanceComponent(DoiComponent):
     super().__init__()
     self.weights = {}
     self.log = pd.DataFrame(np.empty((0, 3)), columns=["timestamp", "mode", "ids"])
-    self.log = self.log.set_index("timestamp")
     self.current_interest = pd.DataFrame(np.empty((0, 2)), columns=["id", "doi"])
-    self.current_interest = self.current_interest.set_index("id")
 
 
   def add_interaction(self, interaction: list):
@@ -23,12 +21,12 @@ class ProvenanceComponent(DoiComponent):
 
 
   def train(self, X: DataFrame):
-    interacted_ids = pd.DataFrame(self.current_interest["id"].astype(np.int64))
-    interacted_dois = pd.DataFrame(self.current_interest["doi"])
+    interacted_ids = pd.DataFrame(X["id"].astype(np.int64))
+    computed_doi = self.compute_doi(X)
 
     X["id"] = X["id"].astype(np.int64)
-    training_data = self.get_values_for_ids(interacted_ids, X)
-    training_labels = interacted_dois.to_numpy()
+    training_data = interacted_ids.merge(X.set_index("id"), on="id").drop(columns=["id"]).to_numpy()
+    training_labels = computed_doi.to_numpy()
 
     self.predictor.fit(training_data, training_labels)
 
@@ -43,25 +41,28 @@ class ProvenanceComponent(DoiComponent):
 
     # aggregate their count across the same interaction technique
     grouped_count = df.groupby(["mode", 'id']).size().reset_index(name='count')
-    grouped_count
 
     # aggregate the count for the same id
-    total_count = grouped_count[["id", "count"]].groupby(["id"]).aggregate("sum")
-    total_count
+    total_count = grouped_count[["id", "count"]].groupby(["id"], as_index="id").aggregate("sum")
 
     # compute doi as relative frequency
     min_count = total_count["count"].min()
     max_count = total_count["count"].max()
     relative_count = (total_count["count"] - min_count) / (max_count - min_count)
     as_matrix = np.array([relative_count.index, relative_count]).transpose()
-    self.current_interest = pd.DataFrame(as_matrix, columns=["id", "doi"])
+    as_df = pd.DataFrame(as_matrix, columns=["id", "doi"])
+    as_df["id"] = as_df["id"].astype(np.int64)
 
-    self.current_interest["id"] = self.current_interest["id"].astype(np.int64)
-
-    # return doi for ids in input
+    # get doi for ids in input
     ids = pd.DataFrame(X["id"].astype(np.int64), columns=["id"])
-    return ids.merge(self.current_interest, on="id")
+    doi = ids.set_index("id").join(as_df.set_index("id"), on="id", lsuffix="left_", rsuffix="right_")["doi"]
+
+    # input might contain items that have not been interacted with, which evaluate to NaN, so set them 0
+    doi.loc[doi.isna()] = 0
+
+    return doi
 
 
   def predict_doi(self, X):
-    return self.predictor.predict(X.to_numy())
+    X_ = X.drop(columns=["id"])
+    return self.predictor.predict(X_.to_numpy())
