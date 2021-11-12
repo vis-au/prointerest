7d6b91ab74ee52bb60bfbd434691d5f53d9ca560
Mahog <6837928+Mahog@users.noreply.github.com>
2021-11-12 10:20:19 +0100

    add no_context strategy

---
+++ server/context_item_selection_strategy/no_context.py
@@ -0,0 +1,6 @@
+from numpy import empty
+from .context_item_selection_strategy import ContextItemSelectionStrategy
+
+class NoContext(ContextItemSelectionStrategy):
+  def get_context_ids(self, current_chunk: int):
+    return empty((0, self.n_dims))
