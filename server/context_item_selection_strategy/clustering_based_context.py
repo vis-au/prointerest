4ed8511b4c3c6dab170b1497914bb5a9d0768819
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-04 11:03:18 +0100

    fix actual data in context

--- server/context_item_selection_strategy/clustering_based_context.py
+++ server/context_item_selection_strategy/clustering_based_context.py
@@ -1,8 +1,7 @@
 from pandas import DataFrame
 from numpy import empty
 from sklearn.cluster import KMeans
-from database import get_from_data
-from database import get_from_processed
+from database import ID, DOI_DB, PROCESSED_DB, get_from_data, get_from_processed
 from .context_item_selection_strategy import ContextItemSelectionStrategy
 
 
@@ -12,7 +11,7 @@ class ClusteringBasedContext(ContextItemSelectionStrategy):
         self.n_clusters = n_clusters
 
     def get_context_items(self, current_chunk: int):
-        data = get_from_processed(["TRUE"], as_df=True)
+        data = get_from_data([f"{DOI_DB}.{ID} IN (SELECT {ID} FROM {PROCESSED_DB})"], as_df=True)
         numeric = data.select_dtypes(["number"]).to_numpy()
         clustering = KMeans(n_clusters=self.n_clusters).fit(numeric)
 
@@ -20,8 +19,6 @@ class ClusteringBasedContext(ContextItemSelectionStrategy):
         representatives = []
 
         for i in range(self.n_clusters):
-            print(data[labels == i].shape)
-            print(data[labels == i])
             # pick first element in that class as a representative (could also pick randomly)
             if len(data[labels == i]) == 0:
                 e = DataFrame(empty((0, self.n_dims)))
