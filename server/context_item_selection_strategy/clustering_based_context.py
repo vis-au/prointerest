7e2628c1f4506258bdc45e89a047207d951bbd0d
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-10 22:17:34 +0100

    use incremental clustering for context

--- server/context_item_selection_strategy/clustering_based_context.py
+++ server/context_item_selection_strategy/clustering_based_context.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 from numpy import empty
-from sklearn.cluster import KMeans
-from database import ID, DOI_DB, PROCESSED_DB, get_from_data, get_from_processed
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from database import ID, CHUNK, get_from_column_data, get_from_processed
 from .context_item_selection_strategy import ContextItemSelectionStrategy
 
 
@@ -9,13 +9,26 @@ class ClusteringBasedContext(ContextItemSelectionStrategy):
     def __init__(self, n_dims: int, n_clusters: int) -> None:
         super().__init__(n_dims)
         self.n_clusters = n_clusters
+        self.clustering = MiniBatchKMeans(n_clusters=self.n_clusters)
 
     def get_context_items(self, current_chunk: int):
-        data = get_from_data([f"{DOI_DB}.{ID} IN (SELECT {ID} FROM {PROCESSED_DB})"], as_df=True)
-        numeric = data.select_dtypes(["number"]).to_numpy()
-        clustering = KMeans(n_clusters=self.n_clusters).fit(numeric)
+        response = get_from_processed(
+            [f"{CHUNK}={current_chunk}"], dimensions=ID,
+            as_df=True
+        )
+        ids_list = response[ID.lower()].values.tolist()
+        ids_list = list(map(str, ids_list))
+        ids_sql = ",".join(ids_list)
 
-        labels = clustering.labels_
+        data = get_from_column_data(
+            [f"{ID} IN ({ids_sql})"],
+            as_df=True
+        )
+        numeric = data.select_dtypes(["number"]).to_numpy()
+        # clustering = KMeans(n_clusters=self.n_clusters).fit(numeric)
+        self.clustering.partial_fit(numeric)
+
+        labels = self.clustering.labels_
         representatives = []
 
         for i in range(self.n_clusters):
