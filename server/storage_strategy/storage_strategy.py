ebb7d3d6921cf0c6a2ecf779c82983e2a451c4c6
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-11 13:53:28 +0100

    add database-like get item function to storage strats

--- server/storage_strategy/storage_strategy.py
+++ server/storage_strategy/storage_strategy.py
@@ -1,17 +1,28 @@
 import pandas as pd
+import duckdb
+
+from database import ID
+
+DF = "stored_db"
 
 
 class StorageStrategy:
     def __init__(self, max_size) -> None:
         self.max_size = max_size  # max number of items in storage
         self.storage = pd.DataFrame()  # current state of the storage
-        self.out = pd.DataFrame()  # data that was discarded from storage last
+        self.cursor = duckdb.connect()
+        self.is_storage_registered = False
 
     def insert_chunk(self, chunk: pd.DataFrame) -> None:
         return None
 
-    def get_storage(self) -> pd.DataFrame:
-        return self.storage
+    def get_items_for_ids(self, ids: list[str], as_df=False):
+        if not self.is_storage_registered:
+            self.cursor.register(DF, self.storage)
+            self.is_storage_registered = True
 
-    def get_out(self) -> pd.DataFrame:
-        return self.out
+        response = self.cursor.execute(
+            f"SELECT * FROM {DF} WHERE {ID} IN {tuple(ids)}"
+        )
+
+        return response.fetchdf() if as_df else response.fetchall()
