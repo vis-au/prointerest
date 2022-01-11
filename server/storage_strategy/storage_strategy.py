0270a848c61e9a842395b083eed2200b00080a17
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-11 12:44:44 +0100

    add description for storage strategy properties

--- server/storage_strategy/storage_strategy.py
+++ server/storage_strategy/storage_strategy.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 class StorageStrategy:
     def __init__(self, max_size) -> None:
-        self.max_size = max_size
-        self.storage = pd.DataFrame()
-        self.out = pd.DataFrame()
+        self.max_size = max_size  # max number of items in storage
+        self.storage = pd.DataFrame()  # current state of the storage
+        self.out = pd.DataFrame()  # data that was discarded from storage last
 
     def insert_chunk(self, chunk: pd.DataFrame) -> None:
         return None
