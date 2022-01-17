623ac6bd67c4c6310150d6007a78230e0baeb664
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-17 19:28:00 +0100

    add binning function to config script

--- server/benchmarks/config.py
+++ server/benchmarks/config.py
@@ -1,5 +1,13 @@
-from doi_component.outlierness_component import OutliernessComponent
+import os
+from os.path import exists
+from sys import path
+import numpy as np
+import pandas as pd
+# make this script "top level"
+cwd = os.getcwd()
+path.append(f"{cwd}/..")
 
+from doi_component.sort_component import SortComponent
 from database import initialize_db, drop_tables
 
 from storage_strategy.no_storage_strategy import *
@@ -20,17 +28,38 @@ from context_item_selection_strategy.sampling_based_context import *
 from context_item_selection_strategy.clustering_based_context import *
 
 
+no_bins = 10
+
+
+# helper function that bins the doi column
+def get_doi_bins_df(doi_df: pd.DataFrame, with_labels=False) -> pd.DataFrame:
+  histogram, edges = np.histogram(doi_df["doi"], bins=no_bins, range=(0, 1))
+  bins_df = pd.DataFrame(histogram.transpose())
+
+  if with_labels:
+    labels = np.digitize(doi_df["doi"], bins=edges)
+    return bins_df, labels
+  else:
+    return bins_df
+
+
+# wipe the databases that track doi, processed, update chunks, etc.
 def reset():
   drop_tables()
   initialize_db(data_path, column_data_path)
 
 
-outlierness = OutliernessComponent(["ratio", "duration"])
+# doi = OutliernessComponent(["ratio", "duration"])
+# doi_label = "outlierness"
+doi = SortComponent(["ratio"])
+doi_label = "sort"
+# doi = DensityComponent(bandwidth=5)
+# doi_label = "density"
 
-total_size = 100000  # total number of processed items, not nec. the size of the data
-chunk_size = 1000  # number of new items retrieved per step
+total_size = 10000  # total number of processed items, not nec. the size of the data
+chunk_size = 50  # number of new items retrieved per step
 chunks = round(total_size / chunk_size)  # number of steps
-max_size = chunk_size * 5  # maximum size of storages
+storage_size = chunk_size * chunks  # maximum size of storages
 
 data_path = "../data/nyc_taxis.shuffled_full.csv.gz"
 column_data_path = "../data/nyc_taxis.shuffled_full.parquet"
@@ -39,23 +68,53 @@ n_dims = 17  # number of dimensions in the data
 
 storage_strategies = [
   ("no_storage_strategy", NoStorageStrategy()),
-  ("compression_strategy", CompressionStrategy(max_size=max_size)),
+  ("compression_strategy", CompressionStrategy(max_size=storage_size)),
   ("progressive_bin_sampler", ProgressiveBinSampler()),
-  ("reservoir_sampling_strategy", ReservoirSamplingStrategy(max_size=max_size)),
-  ("windowing_strateg", WindowingStrategy(max_size=max_size))
+  ("reservoir_sampling_strategy", ReservoirSamplingStrategy(max_size=storage_size)),
+  ("windowing_strategy", WindowingStrategy(max_size=storage_size))
 ]
 
+n_chunks = 10
+max_age = 10
+
 update_strategies = [
-  ("no chunk", NoUpdate(n_dims=n_dims, storage=None)),
-  ("oldest n chunks", OldestChunksUpdate(n_dims=n_dims, storage=None, n_chunks=3, max_age=10)),
-  ("last n chunks", LastNChunksUpdate(n_dims=n_dims, storage=None, n_chunks=3)),
-  ("regular intervals", RegularIntervalUpdate(n_dims=n_dims, storage=None, interval=2, max_age=10)),
-  ("outdated bins", OutdatedBinUpdate(n_dims=n_dims, storage=None))
+  ("no update", NoUpdate(
+    n_dims=n_dims, storage=None
+  )),
+  ("oldest n chunks", OldestChunksUpdate(
+    n_dims=n_dims, storage=None, n_chunks=n_chunks, max_age=max_age
+  )),
+  ("last n chunks", LastNChunksUpdate(
+    n_dims=n_dims, storage=None, n_chunks=n_chunks
+  )),
+  ("regular intervals", RegularIntervalUpdate(
+    n_dims=n_dims, storage=None, interval=n_chunks, max_age=max_age
+  )),
+  # ("outdated bins", OutdatedBinUpdate(n_dims=n_dims, storage=None))
 ]
 
 context_strategies = [
-  ("no context", NoContext(n_dims=n_dims, storage=None)),
-  ("chunk based", RandomChunkBasedContext(n_dims=n_dims, n_chunks=3, storage=None)),
-  ("sampling based", RandomSamplingBasedContext(n_dims=n_dims, n_samples=chunk_size, storage=None)),
-  ("clustering based", ClusteringBasedContext(n_dims=n_dims, n_clusters=chunk_size, storage=None))
+  ("no context", NoContext(
+    n_dims=n_dims, storage=None
+  )),
+  ("chunk based", RandomChunkBasedContext(
+    n_dims=n_dims, n_chunks=n_chunks, storage=None
+  )),
+  ("sampling based", RandomSamplingBasedContext(
+    n_dims=n_dims, n_samples=chunk_size * n_chunks, storage=None
+  )),
+  ("clustering based", ClusteringBasedContext(
+    n_dims=n_dims, n_clusters=chunk_size, storage=None
+  ))
 ]
+
+# create the path for storing the benchmark results if they do not exist
+path = f"./out/{doi_label}/{total_size}/{chunk_size}"
+if not exists("./out"):
+  os.mkdir("./out")
+if not exists(f"./out/{doi_label}"):
+  os.mkdir(f"./out/{doi_label}")
+if not exists(f"./out/{doi_label}/{total_size}"):
+  os.mkdir(f"./out/{doi_label}/{total_size}")
+if not exists(path):
+  os.mkdir(path)
