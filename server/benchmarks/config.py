7a6b94da5c9a0d5bd3286afea8eee926da5df9a4
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-18 14:14:52 +0100

    update benchmarking pipeline

--- server/benchmarks/config.py
+++ server/benchmarks/config.py
@@ -3,11 +3,14 @@ from os.path import exists
 from sys import path
 import numpy as np
 import pandas as pd
+
 # make this script "top level"
 cwd = os.getcwd()
 path.append(f"{cwd}/..")
 
 from doi_component.sort_component import SortComponent
+from doi_component.outlierness_component import OutliernessComponent
+from doi_component.density_component import DensityComponent
 from database import initialize_db, drop_tables
 
 from storage_strategy.no_storage_strategy import *
@@ -28,9 +31,6 @@ from context_item_selection_strategy.sampling_based_context import *
 from context_item_selection_strategy.clustering_based_context import *
 
 
-no_bins = 10
-
-
 # helper function that bins the doi column
 def get_doi_bins_df(doi_df: pd.DataFrame, with_labels=False) -> pd.DataFrame:
   histogram, edges = np.histogram(doi_df["doi"], bins=no_bins, range=(0, 1))
@@ -46,24 +46,50 @@ def get_doi_bins_df(doi_df: pd.DataFrame, with_labels=False) -> pd.DataFrame:
 # wipe the databases that track doi, processed, update chunks, etc.
 def reset():
   drop_tables()
-  initialize_db(data_path, column_data_path)
+  initialize_db(
+    row_data_path=data_path,
+    column_data_path=column_data_path,
+    id_column=id_column,
+    total_size=total_db_size,
+    # process_chunk_callback=taxi_process_chunk
+  )
 
 
-# doi = OutliernessComponent(["ratio", "duration"])
+# compute the ratio and duration in s per ride in the taxi dataset
+def taxi_process_chunk(chunk: pd.DataFrame):
+  dropoff = chunk["tpep_dropoff_datetime"]
+  pickup = chunk["tpep_pickup_datetime"]
+  chunk["duration"] = dropoff - pickup
+  chunk["duration"] = chunk["duration"].apply(lambda x: x.total_seconds())
+  chunk["ratio"] = chunk["tip_amount"] / chunk["total_amount"]
+  return chunk
+
+
+# --- BENCHMARK CONFIGURATION
+# doi = OutliernessComponent(["0", "1"])
 # doi_label = "outlierness"
-doi = SortComponent(["ratio"])
-doi_label = "sort"
-# doi = DensityComponent(bandwidth=5)
-# doi_label = "density"
+# doi = SortComponent(["0"])
+# doi_label = "sort"
+doi = DensityComponent(bandwidth=5)
+doi_label = "density"
 
 total_size = 10000  # total number of processed items, not nec. the size of the data
-chunk_size = 50  # number of new items retrieved per step
+chunk_size = 2  # number of new items retrieved per step
 chunks = round(total_size / chunk_size)  # number of steps
 storage_size = chunk_size * chunks  # maximum size of storages
+no_bins = 10  # number of bins used in doi histograms
 
-data_path = "../data/nyc_taxis.shuffled_full.csv.gz"
+# --- USE CASE CONFIGURATION
+# data_path = "../data/nyc_taxis.shuffled_full.csv.gz"
+data_path = "../data/blobs.csv"
 column_data_path = "../data/nyc_taxis.shuffled_full.parquet"
-n_dims = 17  # number of dimensions in the data
+data_label = "blobs"
+
+id_column = "tripID"
+# total_db_size = 112145904  # full size of database
+total_db_size = 1000000  # full size of database
+# n_dims = 17  # number of dimensions in the data
+n_dims = 2
 
 
 storage_strategies = [
@@ -109,12 +135,14 @@ context_strategies = [
 ]
 
 # create the path for storing the benchmark results if they do not exist
-path = f"./out/{doi_label}/{total_size}/{chunk_size}"
+path = f"./out/{data_label}/{doi_label}/{total_size}/{chunk_size}"
 if not exists("./out"):
   os.mkdir("./out")
-if not exists(f"./out/{doi_label}"):
-  os.mkdir(f"./out/{doi_label}")
-if not exists(f"./out/{doi_label}/{total_size}"):
-  os.mkdir(f"./out/{doi_label}/{total_size}")
+if not exists(f"./out/{data_label}"):
+  os.mkdir(f"./out/{data_label}")
+if not exists(f"./out/{data_label}/{doi_label}"):
+  os.mkdir(f"./out/{data_label}/{doi_label}")
+if not exists(f"./out/{data_label}/{doi_label}/{total_size}"):
+  os.mkdir(f"./out/{data_label}/{doi_label}/{total_size}")
 if not exists(path):
   os.mkdir(path)
