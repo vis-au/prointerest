6debf9d5a512fbad95aae7df9301ea6b5a73b077
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-19 13:32:32 +0100

    extend storage class

--- server/storage_strategy/storage_strategy.py
+++ server/storage_strategy/storage_strategy.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 import duckdb
 
-from database import ID
+from database import ID, CHUNK, get_from_processed
 
 DF = "stored_db"
 
@@ -15,6 +16,31 @@ class StorageStrategy:
 
     def insert_chunk(self, chunk: pd.DataFrame) -> None:
         return None
+
+    def get_available_ids(self) -> pd.Series:
+        if not self.is_storage_registered:
+            return pd.Series()
+
+        return self.storage[ID]
+
+    def get_available_chunks(self) -> np.ndarray:
+        ids = self.get_available_ids().to_numpy()
+
+        if len(ids) == 0:
+            return pd.DataFrame()
+        elif len(ids) == 1:
+            ids += ids
+
+        ids_sql = tuple(ids)
+
+        chunks_for_ids = get_from_processed(
+            [f"{ID} IN {ids_sql}"],
+            dimensions=f"{CHUNK}",
+            distinct=True,
+            as_df=True
+        )
+        chunks = chunks_for_ids[CHUNK.lower()].to_numpy().reshape(-1,)
+        return chunks
 
     def get_items_for_ids(self, ids: list[str], as_df=False):
         if not self.is_storage_registered and len(self.storage) > 0:
@@ -30,3 +56,15 @@ class StorageStrategy:
         )
 
         return response.fetchdf() if as_df else response.fetchall()
+
+    def get_items_for_chunks(self, chunks: list[str], as_df=False):
+        if len(chunks) == 0:
+            return pd.DataFrame()
+
+        if len(chunks) == 1:
+            chunks += chunks
+
+        ids_for_chunks = get_from_processed([f"{CHUNK} in {tuple(chunks)}"], as_df=True)[ID.lower()]
+        ids_for_chunks = ids_for_chunks.to_list()
+        ids_for_chunks = list(map(str, ids_for_chunks))
+        return self.get_items_for_ids(ids_for_chunks, as_df=as_df)
