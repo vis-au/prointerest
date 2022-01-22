b83141839a98c1c379364f44cc30d1d886fb4239
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-22 14:27:23 +0100

    regular interval udpate uses storage for chunks

--- server/outdated_item_selection_strategy/regular_interval_update.py
+++ server/outdated_item_selection_strategy/regular_interval_update.py
@@ -1,6 +1,7 @@
+from numpy import empty
 from .outdated_item_selection_strategy import OutdatedItemSelectionStrategy
 from storage_strategy.storage_strategy import StorageStrategy
-from database import ID, CHUNK, get_from_processed
+from database import ID
 
 
 class RegularIntervalUpdate(OutdatedItemSelectionStrategy):
@@ -14,23 +15,37 @@ class RegularIntervalUpdate(OutdatedItemSelectionStrategy):
     Properties
     ----------
     interval : int
-      The interval in which chunks should be recomputed.
+        The interval in which chunks should be recomputed.
     max_age : int
-      The maximum age of considered chunks, thus the lower `max_age`, the faster the retrieval.
+        The maximum age of considered chunks, thus the lower `max_age`, the faster the retrieval.
     """
 
     def __init__(self, n_dims: int, storage: StorageStrategy, n_chunks: int, max_age: int):
         super().__init__(n_dims, storage)
         self.max_age = max_age
-        self.interval = max_age // n_chunks
+        self.n_chunks = n_chunks
 
     def get_outdated_ids(self, current_chunk: int):
-        res = get_from_processed([
-            f"({current_chunk}-{CHUNK}) % {self.interval}=0",
-            f"{CHUNK} >= {current_chunk - self.max_age}",
-            f"{CHUNK} < {current_chunk}"
-          ],
-          as_df=True
-        )
+        available_chunks = self.storage.get_available_chunks()
+        if len(available_chunks) == 0:
+          return empty((0, self.n_dims))
 
-        return res[ID.lower()].to_numpy()
+        interval = len(available_chunks) // self.n_chunks
+
+        # updates based on location in the storage dataframe happen at the same int index:
+        # [x][][x][][x][] --> ||max_storage||=6 and interval=2
+        # so we update every item that falls into either of these positions
+        # the interval we chose is set so that there are at most n_chunks in there
+        if interval > 0:
+          outdated_chunks = available_chunks[available_chunks % interval == 0]
+        else:
+          outdated_chunks = available_chunks
+
+        outdated_chunks = outdated_chunks[-self.n_chunks:]
+        outdated_items = self.storage.get_items_for_chunks(outdated_chunks.tolist(), as_df=True)
+
+        if len(outdated_items) == 0:
+          return empty((0, self.n_dims))
+
+        outdated_ids = outdated_items[ID].to_numpy()
+        return outdated_ids
