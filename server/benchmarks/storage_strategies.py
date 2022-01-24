792c73e76e69ad12f2b30ab1d67015201dd28abd
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-24 10:11:47 +0100

    fix indentation in storage_strategies.py

--- server/benchmarks/storage_strategies.py
+++ server/benchmarks/storage_strategies.py
@@ -7,9 +7,9 @@ from storage_strategy.windowing_storage import *
 
 def get_storage_strategies(storage_size: int):
   return [
-  ("no_storage_strategy", NoStorage()),
-  ("compression_strategy", CompressionStorage(max_size=storage_size)),
-  ("progressive_bin_sampler", ProgressiveBinSampler()),
-  ("reservoir_sampling_strategy", ReservoirSamplingStorage(max_size=storage_size)),
-  ("windowing_strategy", WindowingStorage(max_size=storage_size))
-]
+    ("no_storage_strategy", NoStorage()),
+    ("compression_strategy", CompressionStorage(max_size=storage_size)),
+    ("progressive_bin_sampler", ProgressiveBinSampler()),
+    ("reservoir_sampling_strategy", ReservoirSamplingStorage(max_size=storage_size)),
+    ("windowing_strategy", WindowingStorage(max_size=storage_size))
+  ]
