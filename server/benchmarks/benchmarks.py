03b47a7a9abc71aab37adad2384f8b8f4570fa38
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-26 17:29:36 +0100

    add function to run all strats for single test case

--- server/benchmarks/benchmarks.py
+++ server/benchmarks/benchmarks.py
@@ -7,8 +7,10 @@ if True:
   path.append(f"{cwd}/..")
 
 import json
+from copy import copy
 from test_case import (get_dataset_config, get_doi_config, get_parameters_config, get_path,
-                       get_strategy_config, create_test_case)
+                       get_strategy_config, create_test_case, generate_strategies,
+                       StrategiesConfiguration)
 
 
 def load_test_case(index: int):
@@ -39,7 +41,40 @@ def load_test_case(index: int):
     path=PATH
   )
 
+def run_single_test_case(index: int):
+  tc = load_test_case(index)
+  print(tc.name, tc.doi_csv_path)
+  tc.run()
 
-tc_ = load_test_case(0)
-print(tc_.name, tc_.doi_csv_path)
-tc_.run()
+
+def run_test_case_for_all_strategies(index: int, all_storages: bool = False):
+  tc = load_test_case(index)
+
+  contexts_, updates_, storages_ = generate_strategies(tc.data, tc.params)
+  total_tcs = len(contexts_)*len(updates_)
+
+  if all_storages:
+    total_tcs *= len(storages_)
+  else:
+    storages_ = [storages_[0]]  # first element is windowing strategy
+
+  completed_tcs = 0
+  for c in contexts_:
+    for u in updates_:
+      for s in storages_:
+        strategy_config = StrategiesConfiguration(
+          name=f"{s[0]}-{u[0]}-{c[0]}",
+          context_strategy=c[1](),
+          update_strategy=u[1](),
+          storage_strategy=s[1]()
+        )
+        tc_ = copy(tc)
+        tc_.strategies = strategy_config
+
+        completed_tcs += 1
+        print(f"({completed_tcs}/{total_tcs}): {tc_.name}")
+        tc_.run()
+
+
+# run_single_test_case(0)
+run_test_case_for_all_strategies(0)
