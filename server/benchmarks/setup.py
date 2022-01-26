a440d374e28f1f9b2f9f6e2c2a5b88555cbf8187
Mahog <6837928+Mahog@users.noreply.github.com>
2022-01-26 12:14:28 +0100

    heavily refactor test_case creation for more reusability

--- server/benchmarks/setup.py
+++ server/benchmarks/setup.py
@@ -7,56 +7,33 @@ import altair as alt
 cwd = os.getcwd()
 path.append(f"{cwd}/..")
 
-from test_case import get_path
+from test_case import (DatasetConfiguration, DoiConfiguration, ParametersConfiguration,
+                       get_dataset_config, get_doi_config, get_parameters_config, get_path,
+                       generate_strategies)
 from doi_components import *
 from storage_strategies import *
 from context_strategies import *
 from update_strategies import *
 
 
+def load_test_case(index: int):
+  TEST_CASES = json.load(open("./test_cases.json"))
+  test_case = TEST_CASES["test_cases"][index]
+
+
 # load benchmark configuration
-PRESETS = json.load(open("./presets.json"))
 doi_label = "averageness"
 data_label = "blobs"
-PARAMETERS = PRESETS["parameters"][1]
+parameter_label = "12k"
 
-# --- DATASET CONFIGURATION
-DATASET = PRESETS["datasets"][data_label]
-data_path = DATASET["data_path"]
-column_data_path = DATASET["column_data_path"]
-total_db_size = DATASET["total_db_size"]  # full size of database
-n_dims = DATASET["n_dims"]  # number of dimensions in the data
-numeric_columns = DATASET["numeric_columns"]  # columns used in the doi functions
-id_column = "tripID"  # TODO: fixed for now
+DATA: DatasetConfiguration = get_dataset_config(data_label)
+DOI_CONFIG: DoiConfiguration = get_doi_config(doi_label, DATA)
+PARAMETERS: ParametersConfiguration = get_parameters_config(parameter_label)
 
-# --- DOI CONFIGURATION
-doi = get_doi_component(doi_label, numeric_columns)
-
-# --- REMAINING PARAMETERS OF THE BENCHMARKS
-total_size = PARAMETERS["total_size"]  # total number of processed items, not nec. the dataset size
-chunk_size = PARAMETERS["chunk_size"]  # number of new items retrieved per step
-n_bins = PARAMETERS["n_bins"]  # number of bins used in doi histograms
-update_size = PARAMETERS["update_size"]
-context_size = PARAMETERS["context_size"]
-n_chunks_context = max(context_size // chunk_size, 1)  # number of chunks considered for context
-n_chunks_update = max(update_size // chunk_size, 1)  # number of chunks considered for updating
-max_age = PARAMETERS["max_age"]  # maximal age of the considered chunks
-update_interval = PARAMETERS["update_interval"]  # number of chunks before a full update occurs
-
-chunks = round(total_size / chunk_size)  # number of steps
-storage_size = chunk_size * max_age  # maximum size of storages
 # create the path for storing the benchmark results if they do not exist
-path = get_path(data_label, doi_label, total_size, chunk_size)
+PATH = get_path(data_label, doi_label, PARAMETERS.total_size, PARAMETERS.chunk_size)
 
-# load strategies
-storage_strategies = get_storage_strategies(storage_size)
-context_strategies = get_context_strategies(n_dims, n_chunks_context, n_bins)
-update_strategies = get_update_strategies(n_dims, n_chunks_update, max_age)
-
-short_test_case_title = f"doi: {doi_label}, items: {total_size}, chunk size: {chunk_size}"
-full_test_case_title = f"{short_test_case_title}, data: {data_label},\n"\
-                       f"upd. size.: {update_size}, cont. size: {context_size} bins: {n_bins},\n"\
-                       f"max age: {max_age}\n"
+context_strategies, update_strategies, storage_strategies = generate_strategies(DATA, PARAMETERS)
 
 # altair visualizations use the data server extension to reduce notebook size
 alt.data_transformers.enable("data_server")
--- server/benchmarks/test_case.py
+++ server/benchmarks/test_case.py
@@ -1,8 +1,13 @@
 import os
-from dataclasses import dataclass
+import json
+from dataclasses import dataclass, field
 
-from progressive_doi_pipeline import ProgressiveDoiPipeline
 from database import initialize_db, drop_tables
+from doi_components import get_doi_component
+from progressive_doi_pipeline import ProgressiveDoiPipeline
+from context_strategies import get_context_strategies
+from update_strategies import get_update_strategies
+from storage_strategies import get_storage_strategies
 
 from doi_component.doi_component import DoiComponent
 from context_item_selection_strategy.context_item_selection_strategy import *
@@ -11,140 +16,251 @@ from outdated_item_selection_strategy.outdated_item_selection_strategy import *
 from outdated_item_selection_strategy.no_update import *
 from storage_strategy.storage_strategy import *
 from storage_strategy.no_storage import NoStorage
-from storage_strategy.windowing_storage import WindowingStorage
+
+PRESETS = json.load(open("./presets.json"))
+
+
+@dataclass
+class StrategiesConfiguration():
+  name: str
+  context_strategy: ContextItemSelectionStrategy
+  update_strategy: OutdatedItemSelectionStrategy
+  storage_strategy: StorageStrategy
+
+
+@dataclass
+class DatasetConfiguration():
+  name: str
+  data_path: str
+  column_data_path: str
+  id_column: str
+  numeric_columns: list[str]  # columns used in the doi functions
+  total_db_size: int  # full size of database
+  n_dims: int  # number of dimensions in the data
+
+
+@dataclass
+class DoiConfiguration():
+  name: str
+  doi: DoiComponent = None
+
+
+@dataclass
+class ParametersConfiguration:
+  name: str
+  chunks: int  # number of steps
+  total_size: int  # total number of processed items, not nec. the dataset size
+  chunk_size: int  # number of new items retrieved per step
+  context_size: int
+  update_size: int
+  storage_size: int  # maximum size of storages
+  max_age: int  # maximal age of the considered chunks
+  update_interval: int  # number of chunks before a full update occurs
+  n_bins: int  # number of bins used in analysis
 
 
 @dataclass
 class TestCase:
   name: str
-  doi: DoiComponent
-  context_strategy: ContextItemSelectionStrategy
-  update_strategy: OutdatedItemSelectionStrategy
-  storage_strategy: StorageStrategy
-  doi_label: str
-  data_label: str
-  data_path: str
-  column_data_path: str
-  id_column: str
-  chunk_size: int
-  context_size: int
-  update_size: int
-  chunks: int
-  total_size: int
-  update_interval: int
-  doi_csv_path: str
-  times_csv_path: str
+  strategies: StrategiesConfiguration
+  data: DatasetConfiguration
+  doi: DoiConfiguration
+  params: ParametersConfiguration
+  doi_csv_path: str = field(default_factory=lambda: None)
+  times_csv_path: str = field(default_factory=lambda: None)
   pipeline: ProgressiveDoiPipeline = None
 
   def run(self) -> None:
-    reset(
-      self.data_path,
-      self.data_label,
-      self.column_data_path,
-      self.id_column,
-      self.total_size,
-    )
+    self.initialize_database()
     self.pipeline = ProgressiveDoiPipeline(
       name=self.name,
-      doi=self.doi,
-      storage_strategy=self.storage_strategy,
-      context_strategy=self.context_strategy,
-      update_strategy=self.update_strategy,
-      chunk_size=self.chunk_size,
-      context_size=self.context_size,
-      update_size=self.update_size,
-      chunks=self.chunks
+      doi=self.doi.doi,
+      storage_strategy=self.strategies.storage_strategy,
+      context_strategy=self.strategies.context_strategy,
+      update_strategy=self.strategies.update_strategy,
+      chunk_size=self.params.chunk_size,
+      context_size=self.params.context_size,
+      update_size=self.params.update_size,
+      chunks=self.params.chunks
     )
     self.pipeline.run(
       doi_csv_path=self.doi_csv_path,
       times_csv_path=self.times_csv_path,
-      update_interval=self.update_interval
+      update_interval=self.params.update_interval
     )
     return self.pipeline
 
-
-def run_ground_truth_test_case(doi_label: str, doi: DoiComponent, data_label: str, n_dims: int,
-                               total_size: int, row_path: str, column_path: str, id_column: str,
-                               path: str):
-  ground_truth_test_case = TestCase(
-    name="__ground_truth__",
-    data_label=data_label,
-    id_column=id_column,
-    data_path=row_path,
-    column_data_path=column_path,
-    doi_label=doi_label,
-    doi=doi,
-    storage_strategy=NoStorage(),
-    context_strategy=NoContext(n_dims, None),
-    update_strategy=NoUpdate(n_dims, None),
-    update_interval=0,
-    chunk_size=total_size,
-    context_size=0,
-    update_size=0,
-    chunks=1,
-    total_size=total_size,
-    doi_csv_path=f"{path}/doi/",
-    times_csv_path=None,
-  )
-  return ground_truth_test_case.run()
+  # wipe the databases that track doi, processed, update chunks, etc.
+  def initialize_database(self) -> None:
+    drop_tables()
+    initialize_db(
+      row_data_path=self.data.data_path,
+      column_data_path=self.data.column_data_path,
+      id_column=self.data.id_column,
+      total_size=self.data.total_db_size,
+      process_chunk_callback=taxi_process_chunk if "taxis" in self.data.name else None
+    )
 
 
-def run_bigger_chunks_test_case(doi_label: str, doi: DoiComponent, data_label: str, n_dims: int,
-                                chunk_size: int, update_size: int, context_size: int,
-                                total_size: int, row_path: str, column_path: str, id_column: str,
-                                path: str):
-  bigger_chunks_test_case = TestCase(
-    name="__bigger_chunks__",
-    doi=doi,
-    data_label=data_label,
-    id_column=id_column,
-    data_path=row_path,
-    column_data_path=column_path,
-    doi_label=doi_label,
-    storage_strategy=NoStorage(),
-    context_strategy=NoContext(n_dims, None),
-    update_strategy=NoUpdate(n_dims, None),
-    update_interval=0,
-    chunk_size=chunk_size + update_size + context_size,
-    context_size=0,
-    update_size=0,
-    chunks=round(total_size // (chunk_size + update_size + context_size)),
-    total_size=total_size,
-    doi_csv_path=f"{path}/doi/",
-    times_csv_path=None
-  )
-  return bigger_chunks_test_case.run()
-
-
-def create_test_case(name: str, data_label: str, doi_label: str, data_path: str,
-                     column_data_path: str, id_column: str, doi: DoiComponent, s: StorageStrategy,
-                     c: ContextItemSelectionStrategy, u: OutdatedItemSelectionStrategy,
-                     chunk_size: int, context_size: int, update_size: int, update_interval: int,
-                     path: str, chunks: int, total_size: int) -> TestCase:
+def create_test_case(name: str, strategies: StrategiesConfiguration,
+                     data: DatasetConfiguration, doi: DoiConfiguration,
+                     params: ParametersConfiguration, path: str) -> TestCase:
 
   doi_csv_path = f"{path}/doi/"
   times_csv_path = f"{path}/times/"
 
   return TestCase(
     name=name,
-    data_label=data_label,
-    data_path=data_path,
-    column_data_path=column_data_path,
-    id_column=id_column,
-    doi_label=doi_label,
+    strategies=strategies,
+    data=data,
     doi=doi,
-    storage_strategy=s,
-    context_strategy=c,
-    update_strategy=u,
-    chunk_size=chunk_size,
-    context_size=context_size,
-    update_size=update_size,
-    chunks=chunks,
-    total_size=total_size,
-    update_interval=update_interval,
+    params=params,
     doi_csv_path=doi_csv_path,
-    times_csv_path=times_csv_path,
+    times_csv_path=times_csv_path
   )
+
+
+def create_ground_truth_test_case(data: DatasetConfiguration, doi: DoiConfiguration,
+                               params: ParametersConfiguration, path: str) -> TestCase:
+
+  s = NoStorage()
+  name = "__ground_truth__"
+
+  total_size = params.total_size
+
+  strategy_config = StrategiesConfiguration(
+    name=name,
+    context_strategy=NoContext(data.n_dims, s),
+    update_strategy=NoUpdate(data.n_dims, s),
+    storage_strategy=s,
+  )
+
+  parameters_config = ParametersConfiguration(
+    name=name,
+    chunks=1,
+    total_size=total_size,
+    chunk_size=total_size,
+    update_size=0,
+    context_size=0,
+    storage_size=0,
+    max_age=0,
+    update_interval=0,
+    n_bins=params.n_bins
+  )
+
+  return create_test_case(
+    name=name,
+    strategies=strategy_config,
+    data=data,
+    doi=doi,
+    params=parameters_config,
+    path=path
+  )
+
+
+def create_bigger_chunks_test_case(data: DatasetConfiguration, doi: DoiConfiguration,
+                                params: ParametersConfiguration, path: str) -> TestCase:
+
+  s = NoStorage()
+  name = "__bigger_chunks__"
+
+  strategy_config = StrategiesConfiguration(
+    name=name,
+    context_strategy=NoContext(data.n_dims, s),
+    update_strategy=NoUpdate(data.n_dims, s),
+    storage_strategy=s,
+  )
+
+  total_size = params.total_size
+  chunk_size = params.chunk_size
+  context_size = params.context_size
+  update_size = params.update_size
+
+  parameters_config = ParametersConfiguration(
+    name=name,
+    chunks=round(total_size // (chunk_size + update_size + context_size)),
+    total_size=total_size,
+    chunk_size=chunk_size + update_size + context_size,
+    update_size=0,
+    context_size=0,
+    storage_size=0,
+    max_age=0,
+    update_interval=0,
+    n_bins=params.n_bins
+  )
+
+  return create_test_case(
+    name=name,
+    strategies=strategy_config,
+    data=data,
+    doi=doi,
+    params=parameters_config,
+    path=path
+  )
+
+
+def get_dataset_config(data_label: str) -> DatasetConfiguration:
+  preset = PRESETS["datasets"][data_label]
+  rows_ = preset["data_path"]
+  columns_ = preset["column_data_path"]
+  data_size = preset["total_db_size"]
+  n_dims = preset["n_dims"]
+  numeric_ = preset["numeric_columns"]
+  id_ = "tripID"  # TODO: static for now
+
+  return DatasetConfiguration(data_label, rows_, columns_, id_, numeric_, data_size, n_dims)
+
+
+def get_doi_config(doi_label: str, data: DatasetConfiguration) -> DoiConfiguration:
+  doi_component = get_doi_component(doi_label, data.numeric_columns)
+
+  return DoiConfiguration(doi_label, doi_component)
+
+
+def get_parameters_config(parameter_label: str) -> ParametersConfiguration:
+  preset = PRESETS["parameters"][parameter_label]
+  total_size = preset["total_size"]
+  chunk_size = preset["chunk_size"]
+  chunks = round(total_size / chunk_size)
+  context_size = preset["context_size"]
+  update_size = preset["update_size"]
+  max_age = preset["max_age"]
+  storage_size = chunk_size * max_age
+  update_interval = preset["update_interval"]
+  n_bins = preset["n_bins"]
+
+  return ParametersConfiguration(parameter_label, chunks, total_size, chunk_size, context_size,
+                                 update_size, storage_size, max_age, update_interval, n_bins)
+
+
+def generate_strategies(data: DatasetConfiguration, params: ParametersConfiguration):
+  context_size = params.context_size
+  chunk_size = params.context_size
+  update_size = params.update_size
+  storage_size = params.storage_size
+  n_dims = data.n_dims
+  n_bins = params.n_bins
+  max_age = params.max_age
+  total_size = params.total_size
+  n_chunks_context = max(context_size // chunk_size, 1)  # number of chunks considered for context
+  n_chunks_update = max(update_size // chunk_size, 1)  # number of chunks considered for updating
+
+  context_strategies = get_context_strategies(n_dims, n_chunks_context, n_bins)
+  update_strategies = get_update_strategies(n_dims, n_chunks_update, max_age)
+  storage_strategies = get_storage_strategies(storage_size)
+
+  return context_strategies, update_strategies, storage_strategies
+
+
+def get_short_title(doi: DoiConfiguration, params: ParametersConfiguration):
+  return f"doi: {doi.name}, items: {params.total_size}, chunk size: {params.chunk_size}"
+
+
+def get_full_title(doi: DoiConfiguration, params: ParametersConfiguration,
+                   data: DatasetConfiguration):
+  return f"{get_short_title(doi, params)}, data: {data.name},\n"\
+          f"|upd.|.: {params.update_size}, |cont.|: {params.context_size},"\
+          f"bins: {params.n_bins}, age: {params.max_age}\n"
 
 
 def get_path(data_label: str, doi_label: str, total_size: int, chunk_size: int) -> str:
@@ -164,52 +280,6 @@ def get_path(data_label: str, doi_label: str, total_size: int, chunk_size: int) 
   return path
 
 
-def run_test_case_from_config(config: dict, context_strategies: list[ContextItemSelectionStrategy],
-                              update_strategies: list[OutdatedItemSelectionStrategy],
-                              skip_if_exists: bool = True) -> None:
-
-  for doi_label in config.doi_functions:
-    for parameters in config.parameters:
-      parameter_label = parameters["parameter_label"]
-      update_interval = parameters["update_interval"]
-      chunk_size = parameters["chunk"]
-      total_size = parameters["total_size"]
-      chunks = round(total_size / chunk_size)
-
-      for d_ in config.datasets:
-        dataset = config.datasets[d_]
-        data_label = dataset["data_label"]
-        path = get_path(data_label, doi_label, total_size, chunk_size)
-
-        for context_ in context_strategies:
-          context_label = context_[0]
-          context_strategy = context_[1]
-
-          for update_ in update_strategies:
-            update_label = update_[0]
-            update_strategy = update_[1]
-
-            label = f"{context_label}-{update_label}"
-            if skip_if_exists and path.isfile(f"{path}/doi/{label}.csv"):
-              print("skipping test case because already completed.")
-              continue
-
-            test_case = create_test_case(
-              # name=f"{doi_label}{data_label}{context_label}{update_label}",
-              name=label,
-              doi_label=doi_label,
-              storage_strategy=WindowingStorage(max_size=total_size),
-              context_strategy=context_strategy,
-              update_strategy=update_strategy,
-              chunk_size=chunk_size,
-              chunks=chunks,
-              update_interval=update_interval,
-              path=path
-            )
-
-            test_case.run()
-
-
 # compute the ratio and duration in s per ride in the taxi dataset
 def taxi_process_chunk(chunk: pd.DataFrame):
   dropoff = chunk["tpep_dropoff_datetime"]
@@ -218,16 +288,3 @@ def taxi_process_chunk(chunk: pd.DataFrame):
   chunk["duration"] = chunk["duration"].apply(lambda x: x.total_seconds())
   chunk["ratio"] = chunk["tip_amount"] / chunk["total_amount"]
   return chunk
-
-
-# wipe the databases that track doi, processed, update chunks, etc.
-def reset(data_path: str, data_label: str, column_data_path: str, id_column: str,
-          total_size: int) -> None:
-  drop_tables()
-  initialize_db(
-    row_data_path=data_path,
-    column_data_path=column_data_path,
-    id_column=id_column,
-    total_size=total_size,
-    process_chunk_callback=taxi_process_chunk if "taxis" in data_label else None
-  )
