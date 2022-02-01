28905ca453bd4fed3faa245e94c8819af838666e
Mahog <6837928+Mahog@users.noreply.github.com>
2022-02-01 18:41:04 +0100

    add composite test case to test_cases.json

--- server/benchmarks/notebook_test_case.py
+++ server/benchmarks/notebook_test_case.py
@@ -7,8 +7,8 @@ from test_case import get_path
 
 # load benchmark configuration
 # available modes: ["strategies", "dois", "datasets", "parameters"]
-MODE = "dois"  # which variable the benchmarks are run over / results are shown by
-TEST_CASE_INDEX = 0
+MODE = "strategies"  # which variable the benchmarks are run over / results are shown by
+TEST_CASE_INDEX = 1
 
 tc = load_test_case(TEST_CASE_INDEX)
 DATA = tc.data
@@ -18,7 +18,7 @@ DOI_CONFIG = tc.doi
 PATH = get_path(DATA.name, DOI_CONFIG.name, PARAMETERS.total_size, PARAMETERS.chunk_size)
 
 CONTEXT_STRATEGIES = get_context_strategies(DATA.n_dims, PARAMETERS.chunks, PARAMETERS.n_bins)
-UPDATE_STRATEGIES = get_update_strategies(DATA.n_dims, PARAMETERS.chunks, PARAMETERS.max_age)
+UPDATE_STRATEGIES = get_update_strategies(DATA.n_dims, PARAMETERS.chunks, PARAMETERS.max_age, PARAMETERS.n_bins)
 
 # altair visualizations use the data server extension to reduce notebook size
 alt.data_transformers.enable("data_server")
