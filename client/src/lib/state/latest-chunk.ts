43d9ae8bb10804b4957cf391f8cd3f5886544693
Mahog <6837928+Mahog@users.noreply.github.com>
2022-11-07 16:03:03 +0100

    only show 'interesting' recent items

--- client/src/lib/state/latest-chunk.ts
+++ client/src/lib/state/latest-chunk.ts
@@ -1,4 +1,12 @@
 import type DataItem from "$lib/types/data-item";
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
+import { doiLimit } from "./doi-limit";
+import { doiValues } from "./doi-values";
 
 export const latestChunk = writable([] as DataItem[]);
+
+export const latestInterestingItems = derived([doiLimit, latestChunk, doiValues], ([$doiLimit, $latestChunk, $doiValues]) => {
+  return $latestChunk.filter((item) => {
+    return $doiValues.get(item.id) > $doiLimit;
+  });
+});
--- client/src/lib/view/layers/visualization/recent-points.svelte
+++ client/src/lib/view/layers/visualization/recent-points.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import { afterUpdate } from "svelte";
   import { currentTransform } from "$lib/state/zoom";
-  import { latestChunk } from "$lib/state/latest-chunk";
-
+  import { latestInterestingItems } from "$lib/state/latest-chunk";
 
   export let width: number;
   export let height: number;
@@ -11,25 +10,19 @@
 
   let canvasElement: HTMLCanvasElement = null;
 
-  function renderBins(ctx: CanvasRenderingContext2D) {
+  function render() {
+    const ctx = canvasElement.getContext("2d");
     ctx.clearRect(0, 0, width, height);
     ctx.fillStyle = color;
 
     const t = $currentTransform;
 
-    $latestChunk.forEach(item => {
+    $latestInterestingItems.forEach(item => {
       ctx.beginPath();
       ctx.arc(t.applyX(item.position.x), t.applyY(item.position.y), radius, 0, 2 * Math.PI);
       ctx.fill();
       ctx.closePath();
     });
-  }
-
-  function render() {
-    const ctx = canvasElement.getContext("2d");
-    renderBins(ctx);
-
-    return null;
   }
 
   afterUpdate(render);
