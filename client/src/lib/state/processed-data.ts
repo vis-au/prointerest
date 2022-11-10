a5b05ae1d22717d49ecacb0fa6b848adf3ef0cac
Mahog <6837928+Mahog@users.noreply.github.com>
2022-11-10 09:43:36 +0100

    use typings for dimensions

--- client/src/lib/state/processed-data.ts
+++ client/src/lib/state/processed-data.ts
@@ -1,7 +1,8 @@
+import type { DOIDimension } from "$lib/types/doi-dimension";
 import { writable } from "svelte/store";
 
 export const processedData = writable([] as number[][]);
 
-export const dimensions = writable([] as string[]);
+export const dimensions = writable([] as DOIDimension[]);
 
 export const totalSize = writable(-1);
