c3f0a39bf17deb75e6293aa8193eef8906d53b4e
Mahog <6837928+Mahog@users.noreply.github.com>
2022-11-30 15:54:04 +0100

    refactor sampled data logic (part 2/n)

--- client/src/lib/state/sampled-data.ts
+++ client/src/lib/state/sampled-data.ts
@@ -1,37 +1,21 @@
 import type { Quadtree } from "d3-quadtree";
-import { derived, writable } from "svelte/store";
+import { writable } from "svelte/store";
 import type DataItem from "$lib/types/data-item";
 import { sample } from "../util/sample";
 import { quadtree } from "./quadtree";
-import { bins } from "./bins";
 
 const STEPS_BETWEEN_RESAMPLING = 10;
-
-const SMALL_SAMPLE_SIZE = 1000;
-const LARGE_SAMPLE_SIZE = 50000;
+const SAMPLE_SIZE = 50000;
 
 let iteration = 0;
-let smallSampleProbabilty = 1;
-let largeSampleProbability = 1;
+let sampleProbability = 1;
 
 let currentQuadtree: Quadtree<DataItem> = null;
 let currentProcessedItems: DataItem[] = [];
 
-let largeRandomSample: DataItem[] = [];
-export const randomDataSubset = writable(largeRandomSample);
+let randomSample: DataItem[] = [];
+export const randomDataSubset = writable(randomSample);
 
-let smallRandomSample: DataItem[] = [];
-export const smallRandomDataSubset = writable(smallRandomSample);
-
-// additional set of items that ensure that every bin in the view is at least represented once.
-export const randomlySampledBinItems = derived(bins, ($bins) => {
-  return $bins
-    .map((bin) => {
-      const probability = bin.length === 1 ? 2 : smallSampleProbabilty;
-      return bin.filter(() => sample(probability));
-    })
-    .flat();
-});
 
 quadtree?.subscribe((newTree) => {
   // check if actual data in the quadtree has changed or whether items were just repositioned.
@@ -39,14 +23,10 @@ quadtree?.subscribe((newTree) => {
   if (iteration % STEPS_BETWEEN_RESAMPLING === 0 || currentQuadtree !== newTree) {
     currentProcessedItems = newTree.data();
     currentQuadtree = newTree;
-    smallSampleProbabilty = SMALL_SAMPLE_SIZE / currentProcessedItems.length;
-    largeSampleProbability = LARGE_SAMPLE_SIZE / currentProcessedItems.length;
+    sampleProbability = SAMPLE_SIZE / currentProcessedItems.length;
 
-    smallRandomSample = currentProcessedItems.filter(() => sample(smallSampleProbabilty));
-    largeRandomSample = currentProcessedItems.filter(() => sample(largeSampleProbability));
-
-    smallRandomDataSubset.set(smallRandomSample);
-    randomDataSubset.set(largeRandomSample);
+    randomSample = currentProcessedItems.filter(() => sample(sampleProbability));
+    randomDataSubset.set(randomSample);
   }
 
   // this function is called numerous times before any data has actually been loaded, so make sure
--- client/src/lib/state/sampled-quadtree.ts
+++ client/src/lib/state/sampled-quadtree.ts
@@ -1,11 +1,11 @@
 import { createQuadtree } from "$lib/util/create-quadtree";
 import { writable } from "svelte/store";
-import { smallRandomDataSubset } from "./sampled-data";
+import { randomDataSubset } from "./sampled-data";
 
 let currentSampledQuadtree = createQuadtree();
 export const sampledQuadtree = writable(currentSampledQuadtree);
 
-smallRandomDataSubset.subscribe((items) => {
+randomDataSubset.subscribe((items) => {
   currentSampledQuadtree = createQuadtree();
   currentSampledQuadtree.addAll(items);
   sampledQuadtree.set(currentSampledQuadtree);
