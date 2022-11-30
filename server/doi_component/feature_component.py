d257406c7bef40830a9fd0dfc0f76d9be1f05535
Mahog <6837928+Mahog@users.noreply.github.com>
2022-11-30 14:13:55 +0100

    add doi component for feature-intervals

---
+++ server/doi_component/feature_component.py
@@ -0,0 +1,58 @@
+from typing import Callable
+import pandas as pd
+import numpy as np
+
+from .doi_component import DoiComponent
+
+
+class FeatureComponent(DoiComponent):
+    weights: dict  # prioritization between the doi dimensions, dim->float with sum=1
+    intervals: dict  # interesting ranges [min, max] dim -> float
+    get_dimensions_in_data: Callable  # overloaded getter function for dimensions in the dataset
+
+    def __init__(
+        self,
+        weights: dict = {},
+        intervals: dict = {},
+        get_dimensions_in_data: Callable = lambda _: [],
+    ):
+        super().__init__()
+        self.weights = weights
+        self.intervals = intervals
+        self.get_dimensions_in_data = get_dimensions_in_data
+
+    def compute_doi(self, X: pd.DataFrame):
+        """
+        Computes the degree of interest per item over the dimensions of interest and accompanying
+        weights. Items that match all intervals get the highest interest, and items that only match
+        some dimensions get a lower score.
+        """
+        # HACK: convert the dimension labels into numbers, because the input data only has indeces
+        dimensions = self.get_dimensions_in_data()
+        subspace = [
+            dimensions.index(dimension) for dimension in list(self.weights.keys())
+        ]
+        X_ = X[subspace]
+        X_mask = np.zeros_like(X_)
+
+        for i, dimension in enumerate(self.weights):
+            dim_index = dimensions.index(dimension)  # get the column name in the dataframe ...
+
+            # assume everything to be interesting if no interval is provided
+            interval = (
+                self.intervals[dimension]
+                if dimension in self.intervals
+                else [float("-inf"), float("inf")]
+            )
+
+            min_value = interval[0]
+            max_value = interval[1]
+
+            # create boolean index whether a row matches the filter for that dimension
+            X_mask[:, i] = (X[dim_index] >= min_value) & (X[dim_index] <= max_value)
+
+            # instead of [0, 1], use the provided weight if the dimension matches
+            X_mask[:, i] = X_mask[:, i] * self.weights[dimension]
+
+        X_mask = X_mask.sum(axis=1)  # sum over weights where rows match filters
+        return X_mask
