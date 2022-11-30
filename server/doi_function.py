9864f7ac63df76c86419f350cececc0e3eea23a2
Mahog <6837928+Mahog@users.noreply.github.com>
2022-11-30 14:15:29 +0100

    use feature doi component to compute chunk interest

--- server/doi_function.py
+++ server/doi_function.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from database import *
 from doi_component.scagnostics_component import *
+from doi_component.feature_component import *
 from context_item_selection_strategy.context_item_selection_strategy import *
 from outdated_item_selection_strategy.outdated_item_selection_strategy import *
 from doi_component.provenance_component import ProvenanceComponent
@@ -17,19 +18,25 @@ STORAGE_SIZE = 100000
 current_chunk = 0
 current_interactions = 0
 
-scagnostics_comp = ScagnosticsComponent([5, 17])
-doi_component: DoiComponent = scagnostics_comp
-provenance_comp = ProvenanceComponent()  # FIXME: unused, but kept for legacy compatibility
-
-storage: StorageStrategy = WindowingStorage(STORAGE_SIZE)
-context: ContextItemSelectionStrategy = DoiBasedContext(n_dims=20, storage=storage, n_bins=25)
-update: OutdatedItemSelectionStrategy = None
-
 # contains the weights for the dimensions used in the DOI function
 DIMENSION_WEIGHTS = {}
 
 # contains the interesting value ranges within each dimension
-DIMENSION_RANGES = {}
+DIMENSION_INTERVALS = {}
+
+scagnostics_comp = ScagnosticsComponent([5, 17])  # FIXME: unused, but kept for compatibility
+provenance_comp = ProvenanceComponent()  # FIXME: unused, but kept for legacy compatibility
+
+feature_comp: DoiComponent = FeatureComponent(
+  weights=DIMENSION_WEIGHTS,
+  intervals=DIMENSION_INTERVALS,
+  get_dimensions_in_data=get_dimensions_in_data
+)
+doi_component: DoiComponent = feature_comp
+
+storage: StorageStrategy = WindowingStorage(STORAGE_SIZE)
+context: ContextItemSelectionStrategy = DoiBasedContext(n_dims=20, storage=storage, n_bins=25)
+update: OutdatedItemSelectionStrategy = None
 
 
 def reset_doi_component():
@@ -93,17 +100,20 @@ def set_dimension_weights(weights: dict):
   for dimension in weights:
     DIMENSION_WEIGHTS[dimension] = weights[dimension]
 
+  feature_comp.weights = DIMENSION_WEIGHTS
+
 
 def set_dimension_range_of_interest(dimension: str, min_value: float, max_value: float):
-  global DIMENSION_RANGES
+  global DIMENSION_INTERVALS
 
   if min_value is None or max_value is None:
     # if the frontend sets the interesting range to INFINITY, this corresponds to None here
     # for simplicity, we treat INFINITY the same as having no interest at all.
-    del DIMENSION_RANGES[dimension]
+    del DIMENSION_INTERVALS[dimension]
     return
 
-  DIMENSION_RANGES[dimension] = [min_value, max_value]
+  DIMENSION_INTERVALS[dimension] = [min_value, max_value]
+  feature_comp.intervals = DIMENSION_INTERVALS
 
 
 def set_doi_classes(classes: int):
@@ -131,7 +141,7 @@ def log_interaction(mode: Literal["brush", "zoom", "select", "inspect"], items: 
     current_interactions, mode, np.array(items)[:, 0]
   ])
   df = pd.DataFrame(items)
-  df = df.drop(columns=[2, 3, 7, 18, 19])
+  df = df.drop(columns=[2, 3, 7, 18, 19])  # non-numerical columns
   df = df.astype(np.float64)
   df["id"] = df.index
 
@@ -144,7 +154,7 @@ UPDATE_INTERVAL: int = 10
 
 def doi_f(chunk_with_context: np.ndarray):
   df = pd.DataFrame(chunk_with_context)
-  df = df.drop(columns=[2, 3, 7, 18, 19])
+  df = df.drop(columns=[2, 3, 7, 18, 19])  # non-numerical columns
   df = df.astype(np.float64)
 
   df["id"] = df.index
@@ -161,6 +171,7 @@ def compute_dois(items: list) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
   context_items = context.get_context_items(CONTEXT_SIZE, current_chunk)
   updated_ids = context_items[ID].tolist() if len(context_items) > 0 else []
+
   X_ = np.concatenate((X, context_items), axis=0)
   dois_with_context = doi_f(X_)
   new_dois = dois_with_context[:len(X)]
