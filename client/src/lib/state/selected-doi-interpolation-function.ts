2f4250ab4c6e417744b8a694204a4d1af29f715b
Mahog <6837928+Mahog@users.noreply.github.com>
2022-12-01 08:51:43 +0100

    remove interpolation function var

--- client/src/lib/state/selected-doi-interpolation-function.ts
+++
@@ -1,4 +0,0 @@
-import type { FunctionName } from "$lib/types/doi-interpolation-function";
-import { writable } from "svelte/store";
-
-export const selectedDoiInterpolationFunction = writable("linear" as FunctionName);
