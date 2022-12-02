a5f7508fc4769a66740720597752a48b24eb039d
Mahog <6837928+Mahog@users.noreply.github.com>
2022-12-02 13:16:49 +0100

    show number of selected items in tooltip

--- client/src/lib/view/main/tooltip.svelte
+++ client/src/lib/view/main/tooltip.svelte
@@ -2,7 +2,8 @@
   import { activeViewMode } from "$lib/state/active-view-mode";
   import { hoveredItems } from "$lib/state/hovered-items";
   import { hoveredScreenPosition } from "$lib/state/hovered-position";
-  import { separateThousands } from "$lib/util/number-transform";
+  import { separateThousands, truncateFloat } from "$lib/util/number-transform";
+  import Row from "$lib/widgets/row.svelte";
 
   let innerWidth: number;
   let innerHeight: number;
@@ -18,15 +19,27 @@
   $: top =
     tooltipHeight / 2 +
     Math.max(padding, Math.min($hoveredScreenPosition[1], innerHeight - tooltipHeight - padding));
+
+  $: selectedCount = $hoveredItems.filter(item => item.selected).length;
+  $: selectedPercentage = truncateFloat((selectedCount / $hoveredItems.length) * 100);
 </script>
 
 <div
   class="tooltip {$hoveredItems.length === 0 || $activeViewMode === 'scatter' ? 'hidden' : ''}"
   bind:clientHeight={tooltipHeight}
   bind:clientWidth={tooltipWidth}
-  style="left:{left}px;top:{top}px">
-  <span>count: </span>
-  <strong>{separateThousands($hoveredItems.length)}</strong>
+  style="left:{left}px;top:{top}px"
+>
+  <Row>
+    <span>count: </span>
+    <strong>{separateThousands($hoveredItems.length)}</strong>
+  </Row>
+  {#if selectedCount > 0}
+    <Row>
+      <span>selected: </span>
+      <strong>{selectedCount} ({selectedPercentage}%)</strong>
+    </Row>
+  {/if}
 </div>
 
 <svelte:window bind:innerWidth bind:innerHeight />
