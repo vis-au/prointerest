6afe4dd9eefb199fa3be6345b0cd3dbac1886899
Mahog <6837928+Mahog@users.noreply.github.com>
2022-12-02 10:43:47 +0100

    refactor dt selection functions into util/

--- client/src/lib/state/selection-in-dt.ts
+++ client/src/lib/state/selection-in-dt.ts
@@ -1,5 +1,5 @@
-import type { DecisionTree, InternalNode } from "$lib/types/decision-tree";
-import type { DimensionFilter } from "$lib/types/steering-filters";
+import type { DecisionTree } from "$lib/types/decision-tree";
+import { doesItemFitFilter, getFiltersForDTPath } from "$lib/util/dt-functions";
 import { derived, writable } from "svelte/store";
 import { doiValues } from "./doi-values";
 import { dimensions } from "./processed-data";
@@ -12,38 +12,7 @@ export const dtFilters = derived([selectionInDT], ([$selectionInDT]) => {
     return null;
   }
 
-  const filters: DimensionFilter = {};
-
-  // transform consecutive nodes into interval filters
-  $selectionInDT.forEach((node) => {
-    const predecessor = $selectionInDT.find(
-      (d) => d.type === "internal" && (d.left === node || d.right === node)
-    ) as InternalNode;
-
-    if (!predecessor) {
-      return;
-    }
-
-    if (predecessor.left === node) {
-      const interval = filters[predecessor.feature] || [null, null];
-      interval[1] = predecessor.threshold;
-      filters[predecessor.feature] = interval;
-    }
-    if (predecessor.right === node) {
-      const interval = filters[predecessor.feature] || [null, null];
-      interval[0] = predecessor.threshold;
-      filters[predecessor.feature] = interval;
-    }
-  });
-
-  // if both sides of a subtree are included, remove the filter
-  for (const dimension in filters) {
-    if (filters[dimension][0] === filters[dimension][1]) {
-      delete filters[dimension];
-    }
-  }
-
-  return filters;
+  return getFiltersForDTPath($selectionInDT);
 });
 
 export const visibleItemsSelectedInDT = derived(
@@ -53,22 +22,6 @@ export const visibleItemsSelectedInDT = derived(
       return [];
     }
 
-    return $visibleInterestingData.filter((item) => {
-      let matchesFilters = true;
-
-      // apply the filters defined by the brushes in the
-      Object.keys($dtFilters).forEach((dimension) => {
-        const index = $dimensions.indexOf(dimension);
-
-        if ($dtFilters[dimension][0]) {
-          matchesFilters = matchesFilters && item.values[index] >= $dtFilters[dimension][0];
-        }
-        if ($dtFilters[dimension][1]) {
-          matchesFilters = matchesFilters && item.values[index] <= $dtFilters[dimension][1];
-        }
-      });
-
-      return matchesFilters;
-    });
+    return $visibleInterestingData.filter(d => doesItemFitFilter(d, $dtFilters, $dimensions));
   }
 );
---
+++ client/src/lib/util/dt-functions.ts
@@ -0,0 +1,74 @@
+import type DataItem from "$lib/types/data-item";
+import type { DecisionTree, InternalNode } from "$lib/types/decision-tree";
+import type { DOIDimension } from "$lib/types/doi-dimension";
+import type { DimensionFilter } from "$lib/types/steering-filters";
+
+/**
+ * Transforms a list of decision tree nodes into an object defining the per-dimension filters
+ * corresponding to the nodes visited in that path.
+ * @param dtPath list of internal and leaf nodes from a regression/fdl tree
+ * @returns the record of filters
+ */
+export function getFiltersForDTPath(dtPath: DecisionTree[]) {
+  const filters: DimensionFilter = {};
+
+  // transform consecutive nodes into interval filters
+  dtPath.forEach((node) => {
+    const predecessor = dtPath.find(
+      (d) => d.type === "internal" && (d.left === node || d.right === node)
+    ) as InternalNode;
+
+    if (!predecessor) {
+      return;
+    }
+
+    if (predecessor.left === node) {
+      const interval = filters[predecessor.feature] || [null, null];
+      interval[1] = predecessor.threshold;
+      filters[predecessor.feature] = interval;
+    }
+    if (predecessor.right === node) {
+      const interval = filters[predecessor.feature] || [null, null];
+      interval[0] = predecessor.threshold;
+      filters[predecessor.feature] = interval;
+    }
+  });
+
+  // if both sides of a subtree are included, remove the filter
+  for (const dimension in filters) {
+    if (filters[dimension][0] === filters[dimension][1]) {
+      delete filters[dimension];
+    }
+  }
+
+  return filters;
+}
+
+/**
+ * Applies the provided dimension filters to an item an returns, whether or not it matches them.
+ * @param item a DataItem from the dataset
+ * @param filter a DimensionFilter defining min-max range for a set of dimensions from the dataset.
+ * @param dimensions the indeces of the dimension names in the item.values property
+ * @returns whether or not the item fits all min-max ranges across all filtered dimensions.
+ */
+export function doesItemFitFilter(
+  item: DataItem,
+  filter: DimensionFilter,
+  dimensions: DOIDimension[]
+) {
+  let matchesFilters = true;
+
+  // apply the filters defined by the brushes in the
+  Object.keys(filter).forEach((dimension) => {
+    const index = dimensions.indexOf(dimension);
+
+    if (filter[dimension][0]) {
+      matchesFilters = matchesFilters && item.values[index] >= filter[dimension][0];
+    }
+    if (filter[dimension][1]) {
+      matchesFilters = matchesFilters && item.values[index] <= filter[dimension][1];
+    }
+  });
+
+  return matchesFilters;
+}
