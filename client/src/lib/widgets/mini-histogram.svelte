887936be694a7349c0a29f0f9b3d7ea412cb2cbc
Mahog <6837928+Mahog@users.noreply.github.com>
2022-12-06 09:47:37 +0100

    add mini histogram widget

---
+++ client/src/lib/widgets/mini-histogram.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { max, scaleLinear } from "d3";
+
+  export let id: string;
+  export let width = 100;
+  export let height = 20;
+  export let values: number[];
+  export let style = "";
+
+  $: scaleX = scaleLinear([0, max(values)], [0, height]);
+</script>
+
+<div {id} class="mini-histogram" {style}>
+  {#each values as value}
+    <div
+      class="bin"
+      style:width="{width / values.length}px"
+      style:height="{scaleX(value)}px"
+    />
+  {/each}
+</div>
+
+<style>
+  div.mini-histogram {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: baseline;
+  }
+  div.mini-histogram .bin {
+    box-sizing: border-box;
+    border: 1px solid #333;
+    background: white;
+  }
+</style>
