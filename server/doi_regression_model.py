219d70d85e4b42ad0747e249c8cfb871c9bcb1f6
Mahog <6837928+Mahog@users.noreply.github.com>
2023-02-23 14:20:17 +0100

    implement outdated item detection

--- server/doi_regression_model.py
+++ server/doi_regression_model.py
@@ -1,5 +1,6 @@
 import math
 from dataclasses import dataclass
+from copy import deepcopy
 
 import numpy as np
 import pandas as pd
@@ -106,13 +107,16 @@ class DoiRegressionModel:
     update_all_dois_after_retraining: bool = (
         True  # should "old" values be updated with new model?
     )
-    column_labels: "list[str]" = []
+    trained_column_labels: "list[str]" = []
+    all_column_labels: "list[str]" = []
+    outdated_items: pd.DataFrame
+    updated_dois: np.ndarray
 
     def __init__(
         self,
         storage: StorageStrategy,
         max_depth: int = 3,
-        validity_threshold: float = 0.8,
+        validity_threshold: float = 0.95,
         interest_threshold: int = 0.75,
     ) -> None:
         self.storage = storage
@@ -121,11 +125,11 @@ class DoiRegressionModel:
         self.interest_threshold = interest_threshold
 
     def __str__(self) -> str:
-        return str(_tree_to_json(self.tree, self.column_labels))
+        return str(_tree_to_json(self.tree, self.trained_column_labels))
 
     def _get_leaf_nodes(self):
         """Simple wrapper function for getting to the leaf nodes."""
-        tree_as_dict = _tree_to_json(self.tree, self.column_labels)
+        tree_as_dict = _tree_to_json(self.tree, self.trained_column_labels)
         leaf_nodes = get_leaf_nodes(tree_as_dict)
         return leaf_nodes
 
@@ -133,7 +137,6 @@ class DoiRegressionModel:
         """
         Evaluate the regression model by comparing its prediction against actual DOI values (R**2).
         """
-
         score = self.score(new_df, new_dois)
         return score >= self.validity_threshold
 
@@ -163,7 +166,7 @@ class DoiRegressionModel:
     def _get_min_max_context(self, n: int) -> pd.DataFrame:
         """Samples half from the leaf with maximum value and half from leaf with minimum value."""
         n_items_per_leaf = math.ceil(n / 2)
-        tree_as_dict = _tree_to_json(self.tree, self.column_labels)
+        tree_as_dict = _tree_to_json(self.tree, self.trained_column_labels)
 
         min_doi_leaf = get_least_interesting_leaf_node(tree_as_dict)
         max_doi_leaf = get_most_interest_leaf_node(tree_as_dict)
@@ -175,10 +178,36 @@ class DoiRegressionModel:
 
         return context_items
 
+    def _get_outdated_items(
+        self, new_tree: DecisionTreeRegressor, old_tree: DecisionTreeRegressor
+    ) -> pd.DataFrame:
+        """Identify those branches in the tree model that are no longer valid (error too large) and
+        return all items that match them."""
+        leaf_nodes = self._get_leaf_nodes()
+        SAMPLE_SIZE = 1000
+
+        outdated_df = pd.DataFrame([], columns=self.all_column_labels)
+
+        # determine, which items are affected by this retraining
+        for leaf_node in leaf_nodes:
+            sample = self._get_n_items_for_leaf_node(SAMPLE_SIZE, leaf_node)
+            score = self.score(
+                sample, new_tree.predict(sample.select_dtypes(include=[np.number]))
+            )
+
+            if score < self.validity_threshold:
+                # has to include the ID dimension for retrieval in upstream functions
+                outdated_df = pd.concat([outdated_df, sample], ignore_index=True)
+
+        return outdated_df
+
     def _train(self, df: pd.DataFrame, dois: np.ndarray) -> None:
         assert df.shape[0] == len(dois)
+
+        self.all_column_labels = df.columns
         numeric_df = df.select_dtypes(include=[np.number])
-        self.column_labels = numeric_df.columns
+        self.trained_column_labels = numeric_df.columns
+        outdated_tree = deepcopy(self.tree)
 
         if self.include_previous_chunks_in_training and hasattr(self.tree, "tree_"):
             # FIXME: use context_size parameter
@@ -187,11 +216,13 @@ class DoiRegressionModel:
             context_dois = self.predict_doi(context_df).reshape((-1,))
             dois = dois.reshape((-1,))
 
-            training_df = pd.concat([numeric_df, context_df])
+            training_df = pd.concat([numeric_df, context_df], ignore_index=True)
             training_dois = np.concatenate([dois, context_dois], axis=0)
             self.tree.fit(training_df, training_dois)
         else:
             self.tree.fit(numeric_df, dois)
+
+        return self.tree, outdated_tree
 
     def _repredict_all_dois(self) -> None:
         outdated_items = self.storage.get_available_items()
@@ -200,17 +231,42 @@ class DoiRegressionModel:
 
         new_dois = self.tree.predict(outdated_items)
         update_dois(ids=outdated_ids.tolist(), dois=new_dois.tolist())
+
         return outdated_ids, new_dois
+
+    def _repredict_outdated_dois(
+        self, updated_tree: DecisionTreeRegressor, outdated_tree: DecisionTreeRegressor
+    ) -> None:
+        outdated_df = self._get_outdated_items(updated_tree, outdated_tree)
+        outdated_ids = outdated_df[ID]
+        outdated_df = outdated_df.select_dtypes(include=[np.number])
+        new_dois = np.array([]).reshape((-1,))
+
+        if len(outdated_df) > 0:
+            new_dois = self.predict_doi(outdated_df)
+            update_dois(ids=outdated_ids.tolist(), dois=new_dois.tolist())
+
+        return outdated_df, new_dois
 
     def update(
         self, new_df: pd.DataFrame, new_dois: np.ndarray, update_outdated: bool = False
     ) -> None:
-        self._train(new_df, new_dois)
+        updated_tree, outdated_tree = self._train(new_df, new_dois)
         if update_outdated:
-            self._repredict_all_dois()
+            # self._repredict_all_dois(updated_tree, outdated_tree)
+            outdated_df, new_dois = self._repredict_outdated_dois(
+                updated_tree, outdated_tree
+            )
+            self.outdated_items = outdated_df
+            self.updated_dois = new_dois
 
     def score(self, new_df: pd.DataFrame, new_dois: np.ndarray):
         assert new_df.shape[0] == len(new_dois)
+
+        # R^2 scores is not well-defined with less than two samples
+        if len(new_df) < 2:
+            return 1
+
         numeric_df = new_df.select_dtypes(include=[np.number])
         return self.tree.score(numeric_df, new_dois)
 
@@ -223,39 +279,3 @@ class DoiRegressionModel:
             return self._get_stratified_context(n)
         elif strategy == "minmax":
             return self._get_min_max_context(n)
-
-    def get_outdated_items(
-        self, new_df: pd.DataFrame, new_dois: np.ndarray
-    ) -> pd.DataFrame:
-        """
-        Identify those branches in the tree model that are no longer valid (error too large) and
-        return all items that match them.
-        """
-
-        # if the model overall is still valid, no need to do anything
-        if not self._is_still_valid(new_df, new_dois):
-            return []
-
-        leaf_nodes = self._get_leaf_nodes()
-        SAMPLE_SIZE = 1000
-
-        # TODO: get the data assigned to each leaf node and score the prediction
-        # TODO: return a list of all leaf nodes that are performing below the threshold
-
-        new_tree = self.tree.copy()
-
-        # TODO: train on new data+context!
-        new_tree.train(new_df, new_dois)
-
-        outdated_df = pd.DataFrame([], columns=new_df.columns)
-
-        # determine, which items are affected by this retraining
-        for leaf_node in leaf_nodes:
-            sample = self._get_n_items_for_leaf_node(SAMPLE_SIZE, leaf_node)
-            score = self.score(sample, new_tree.predict(sample))
-
-            if score < self.validity_threshold:
-                outdated_df = pd.concat(outdated_df, sample, ignore_index=True)
-
-        self.tree = new_tree
-        return outdated_df
